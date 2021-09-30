eefee1507eb129d88573c56127a6764de28052a6
Jiexin Shi <jasonmayday@gmail.com>
2021-09-30 12:35:51 -0400

    Update 202_快乐数.py

--- 202_快乐数.py
+++ 202_快乐数.py
@@ -24,3 +24,28 @@ https://leetcode-cn.com/problems/happy-number/
 输出：false
 
 '''
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def calculate_happy(num):
+            sum_ = 0
+            
+            # 从个位开始依次取，平方求和
+            while num:
+                sum_ += (num % 10) ** 2   # += 两个值相加，然后返回值给符号左侧的变量。  % 返回除法的余数
+                num = num // 10  # // 取整除, 返回商的整数部分
+            return sum_
+
+        # 记录中间结果
+        record = set()
+
+        while True:
+            n = calculate_happy(n)
+            if n == 1:
+                return True
+            
+            # 如果中间结果重复出现，说明陷入死循环了，该数不是快乐数
+            if n in record:
+                return False
+            else:
+                record.add(n)
