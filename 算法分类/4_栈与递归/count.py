b3f632e69556c5b1ace7591712b6ddd41dec8d32
Jiexin Shi <jasonmayday@gmail.com>
2021-09-30 16:13:03 -0400

    Create count.py

---
+++ 算法分类/4_栈与递归/count.py
@@ -0,0 +1,15 @@
+'''
+编写一个递归函数来计算列表包含的元素数。
+'''
+
+arr = [1,2,3,4,5]
+
+def count(arr):
+    if arr  == []:    # 基线条件：数组为空
+        return 0
+#    elif len(arr) == 1:    # 基线条件：数组内只有一个元素(可省略)
+#        return 1
+    else:
+        return 1 + count(arr[1:])    # 递归条件：计算数组中第一个元素外剩余元素个数，再加一
+
+print (count(arr))
