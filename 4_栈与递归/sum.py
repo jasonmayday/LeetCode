d7ea53f73ddabf8e147b871e7de2d25b78e2700e
Jiexin Shi <jasonmayday@gmail.com>
2021-09-30 15:54:10 -0400

    Update sum.py

--- 4_栈与递归/sum.py
+++ 4_栈与递归/sum.py
@@ -1,6 +1,7 @@
 '''
 请编写前述sum函数的代码。（递归求和，基线条件为数组空）
 '''
+arr = [1,2,3,4,5]
 
 def sum(arr):
     if arr  == []:    # 基线条件：数组为空
@@ -10,4 +11,4 @@ def sum(arr):
     else:
         return arr[0] + sum(arr[1:])
 
-print (sum([1,2,3,4,5]))
+print (sum(arr))
