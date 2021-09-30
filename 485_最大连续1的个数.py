661b0bb0c57b40f8bc6091d359f604ee8320fc2f
Jiexin Shi <jasonmayday@gmail.com>
2021-09-30 11:21:10 -0400

    Update 485_最大连续1的个数.py

--- 485_最大连续1的个数.py
+++ 485_最大连续1的个数.py
@@ -10,6 +10,22 @@ https://leetcode-cn.com/problems/max-consecutive-ones/
 
 '''
 
+from typing import List
 
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        maxCount = count = 0
+
+        for i, num in enumerate(nums):
+            if num == 1:
+                count += 1  # 两个值相加，然后返回值给符号左侧的变量。在这里 count = count + 1
+            else:
+                maxCount = max(maxCount, count)
+                count = 0
+        
+        maxCount = max(maxCount, count)
+        return maxCount
+
+sol = Solution()
+result = sol.findMaxConsecutiveOnes([1,1,0,1,1,1,0,0,1,1])
+print(result)
