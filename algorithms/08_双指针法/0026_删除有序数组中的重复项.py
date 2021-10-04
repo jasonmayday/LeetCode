a355957e65085d7788cba633d6c897e7ad0b1c41
Jiexin Shi <jasonmayday@gmail.com>
2021-10-04 12:08:38 -0400

    Update 0026_删除有序数组中的重复项.py

--- algorithms/08_双指针法/0026_删除有序数组中的重复项.py
+++ algorithms/08_双指针法/0026_删除有序数组中的重复项.py
@@ -3,23 +3,6 @@
 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
 
 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
-
-说明:
-
-为什么返回数值是整数，但输出的答案是数组呢?
-
-请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
-
-你可以想象内部操作如下:
-
-// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
-int len = removeDuplicates(nums);
-
-// 在函数里修改输入数组对于调用者是可见的。
-// 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
-for (int i = 0; i < len; i++) {
-    print(nums[i]);
-}
  
 示例 1：
 输入：nums = [1,1,2]
@@ -32,3 +15,25 @@ for (int i = 0; i < len; i++) {
 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
 
 '''
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        fast = slow = 1   # 删除重复元素之后也至少剩下一个元素
+        while fast < n:
+            if nums[fast] != nums[fast - 1]:  # 说明nums[fast] 和之前的元素都不同
+                nums[slow] = nums[fast]       # nums[fast] 的值复制到 nums[slow]
+                slow += 1
+            fast += 1
+        
+        return slow    # 从nums[0]到nums[slow−1]的每个元素都不相同
+
+sol = Solution()
+result = sol.removeDuplicates(nums)
+print(result)
