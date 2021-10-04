9272af1d067fb0cb5fc19a9fe81f31171eac07f3
Jiexin Shi <jasonmayday@gmail.com>
2021-10-03 21:10:59 -0400

    Create 广度优先搜索.py

---
+++ 算法分类/10_图与搜索/广度优先搜索.py
@@ -0,0 +1,12 @@
+
+
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
