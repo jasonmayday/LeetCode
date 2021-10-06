7a2b93b283ad3cd39e28e24e15b44e83799f964b
Jiexin Shi <jasonmayday@gmail.com>
2021-10-06 15:48:34 -0400

    Create 0434_字符串中的单词数.py

---
+++ leetcode_solutions/0434_字符串中的单词数.py
@@ -0,0 +1,27 @@
+'''
+https://leetcode-cn.com/problems/number-of-segments-in-a-string/
+
+统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+
+请注意，你可以假定字符串里不包括任何不可打印的字符。
+
+示例:
+
+输入: "Hello, my name is John"
+输出: 5
+解释: 这里的单词是指连续的不是空格的字符，所以 "Hello," 算作 1 个单词。
+
+'''
+s = "Hello, my name is John"
+
+class Solution:
+    def countSegments(self,s):
+        ans, words = 0, s.split(' ')
+        for word in words:
+            if word != '':
+                ans += 1
+        return ans
+
+sol = Solution()
+result = sol.countSegments(s)
+print (result)  
