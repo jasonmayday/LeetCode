e115e415c180af9975f23ce3fb9f10b32aa55291
Jiexin Shi <jasonmayday@gmail.com>
2021-10-08 10:14:02 -0400

    Update 0187_重复的DNA序列.py

--- leetcode_solutions/0187_重复的DNA序列.py
+++ leetcode_solutions/0187_重复的DNA序列.py
@@ -21,14 +21,7 @@ nums = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
 
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        ans = []
-        cnt = defaultdict(int)
-        for i in range(len(s) - L + 1):
-            sub = s[i : i + L]
-            cnt[sub] += 1
-            if cnt[sub] == 2:
-                ans.append(sub)
-        return ans
+        return [t for t, v in Counter(s[i: i + 10] for i in range(len(s) - 9)).items() if v > 1]
 
 sol = Solution()
 result = sol.findRepeatedDnaSequences(nums)
