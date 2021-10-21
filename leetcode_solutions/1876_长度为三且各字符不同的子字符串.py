d187fe319dee9ad7458434aecff6e965b5f9033a
Jiexin Shi <jasonmayday@gmail.com>
2021-10-21 10:00:04 -0400

    Create 1876_长度为三且各字符不同的子字符串.py

---
+++ leetcode_solutions/1876_长度为三且各字符不同的子字符串.py
@@ -0,0 +1,38 @@
+'''
+https://leetcode-cn.com/problems/substrings-of-size-three-with-distinct-characters/
+
+如果一个字符串不含有任何重复字符，我们称这个字符串为 好 字符串。
+
+给你一个字符串 s ，请你返回 s 中长度为 3 的 好子字符串 的数量。
+
+注意，如果相同的好子字符串出现多次，每一次都应该被记入答案之中。
+
+子字符串 是一个字符串中连续的字符序列。
+
+示例 1：
+输入：s = "xyzzaz"
+输出：1
+解释：总共有 4 个长度为 3 的子字符串："xyz"，"yzz"，"zza" 和 "zaz" 。
+唯一的长度为 3 的好子字符串是 "xyz" 。
+
+示例 2：
+输入：s = "aababcabc"
+输出：4
+解释：总共有 7 个长度为 3 的子字符串："aab"，"aba"，"bab"，"abc"，"bca"，"cab" 和 "abc" 。
+好子字符串包括 "abc"，"bca"，"cab" 和 "abc" 。
+
+'''
+s = "bcsffssnsjjuttnnssppkenengghhqqlzzllpafjsdahqoihaklfhkhhss"
+
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        res = 0
+        n = len(s)
+        for i in range(n - 2):
+            if s[i] != s[i+1] and s[i] != s[i+2] and s[i+1] != s[i+2]:
+                res += 1
+        return res
+        
+sol = Solution()
+result = sol.countGoodSubstrings(s)
+print (result)  
