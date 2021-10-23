1fe126fa00567d04f481a20367a67c9a16c6d4b4
Jiexin Shi <jasonmayday@gmail.com>
2021-10-22 21:36:56 -0400

    Create 0953_验证外星语词典.py

---
+++ leetcode_solutions/0953_验证外星语词典.py
@@ -0,0 +1,48 @@
+'''
+https://leetcode-cn.com/problems/verifying-an-alien-dictionary/
+
+某种外星语也使用英文小写字母，但可能顺序 order 不同。字母表的顺序（order）是一些小写字母的排列。
+
+给定一组用外星语书写的单词 words，以及其字母表的顺序 order，只有当给定的单词在这种外星语中按字典序排列时，返回 true；否则，返回 false。
+
+示例 1：
+输入：words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+输出：true
+解释：在该语言的字母表中，'h' 位于 'l' 之前，所以单词序列是按字典序排列的。
+
+示例 2：
+输入：words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+输出：false
+解释：在该语言的字母表中，'d' 位于 'l' 之后，那么 words[0] > words[1]，因此单词序列不是按字典序排列的。
+
+示例 3：
+输入：words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+输出：false
+解释：当前三个字符 "app" 匹配时，第二个字符串相对短一些，然后根据词典编纂规则 "apple" > "app"，因为 'l' > '∅'，其中 '∅' 是空白字符，定义为比任何其他字符都小（更多信息）。
+
+'''
+from typing import List
+words = ["hello","leetcode"]
+order = "hlabcdefgijkmnopqrstuvwxyz"
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_index = {c:i for i,c in enumerate(order)}
+
+        for i in range(len(words) -1):
+            word1 = words[i]
+            word2 = words[i+1]
+
+            for j in range(min(len(word1), len(word2))):  # 寻找不同单词中第一个不同的字母
+                if word1[j] != word2[j]:   # 一旦出现不同的字母，开始比较
+                    if order_index[word1[j]] > order_index[word2[j]]:
+                        return False
+                    break   # break了，else就不执行
+            else:   # 前面一直一样，长的应当排在后面
+                if len(word1) > len(word2):
+                    return False
+        return True
+
+sol = Solution()
+result = sol.isAlienSorted(words, order)
+print (result)
