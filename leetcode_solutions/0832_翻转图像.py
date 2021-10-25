f7f4d5157b73e017bb7f1d29a957bf3a79c46351
Jiexin Shi <jasonmayday@gmail.com>
2021-10-25 10:44:21 -0400

    Update 0832_翻转图像.py

--- leetcode_solutions/0832_翻转图像.py
+++ leetcode_solutions/0832_翻转图像.py
@@ -22,13 +22,17 @@ from typing import List
 A = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
 
 class Solution:
-    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
-        for row in A:       
-            for k,_ in enumerate(row): row[k] = 1 - row[k]        # Python 风格的循环, 1和0反转
-            i, j = 0, len(row) - 1
-            while i < j:
-                row[i], row[j] = row[j], row[i] # Python 特有的交换
-                i, j = i + 1, j - 1
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rows = len(A)      #  矩阵行数
+        cols = len(A[0])   #  矩阵列数
+        for row in range(rows):
+            A[row] = A[row][::-1]    #  [::-1] 顺序相反操作
+            for col in range(cols):
+                A[row][col] ^= 1     #  
         return A
         
 sol = Solution()
