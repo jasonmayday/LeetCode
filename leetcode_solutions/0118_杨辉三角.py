c65c9159d67d97131e5baea39d43f7c6e83edeaf
Jiexin Shi <jasonmayday@gmail.com>
2021-10-26 16:15:14 -0400

    Create 0118_杨辉三角.py

---
+++ leetcode_solutions/0118_杨辉三角.py
@@ -0,0 +1,37 @@
+'''
+https://leetcode-cn.com/problems/pascals-triangle/
+
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+示例 1:
+输入: numRows = 5
+输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+示例 2:
+输入: numRows = 1
+输出: [[1]]
+
+提示:
+1 <= numRows <= 30
+
+'''
+numRows = 15
+from typing import List
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        ret = list()
+        for i in range(numRows):
+            row = list()
+            for j in range(0, i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(ret[i - 1][j] + ret[i - 1][j - 1])
+            ret.append(row)
+        return ret
+
+sol = Solution()
+result = sol.generate(numRows)
+print(result)
