3d462948a88ce482b12f039c0f372d47e34aa4a5
Jiexin Shi <jasonmayday@gmail.com>
2021-10-26 10:10:05 -0400

    Update 0897_递增顺序搜索树.py

--- leetcode_solutions/0897_递增顺序搜索树.py
+++ leetcode_solutions/0897_递增顺序搜索树.py
@@ -18,16 +18,9 @@ https://assets.leetcode.com/uploads/2020/11/17/ex2.jpg
 0 <= Node.val <= 1000
 
 '''
-root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# 中序遍历：左子树 ---> 根结点 ---> 右子树
 
-#  中序遍历：左子树 ---> 根结点 ---> 右子树
 #  先中序遍历，把结果放在数组中；
 #  然后修改数组中每个节点的左右指针：把节点的左指针设置为 null，把节点的右指针设置为数组的下一个节点。
 
@@ -35,24 +28,34 @@ root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
 #  在链表题目中，我们为了防止链表的头结点发生变化之后，不好维护头结点，我们设置 dummy 从而保证头结点不变。
 #  这个题目中设置了 dummy ，从而保证了在新的树中，dummy 是根节点，最终返回的时候，要返回的是 dummy.right。
 
+root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+# Definition for a binary tree node.
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        self.res = []
-        self.inOrder(root)
-        if not self.res:
-            return 
-        dummy = TreeNode(-1)
-        cur = dummy
-        for node in self.res:
-            node.left = node.right = None
-            cur.right = node
-            cur = cur.right
-        return dummy.right
-    
-    def inOrder(self, root):
-        if not root:
-            return
-        self.inOrder(root.left)
-        self.res.append(root)
-        self.inOrder(root.right)
+        data = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            data.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        temp_root = TreeNode()
+        new_root = temp_root
+        for c in data:
+            temp = TreeNode(val = c)
+            temp_root.right = temp
+            temp_root = temp
+        return new_root.right
 
+
+sol = Solution()
+result = sol.increasingBST(root)
+print(result)
