bb2a3e96fcd4923823e68bf9aacdc16a667e7b91
Jiexin Shi <jasonmayday@gmail.com>
2021-10-31 18:43:17 -0400

    Create 选择排序.py

---
+++ algorithms/17_排序算法/选择排序.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    import random
+    random.seed(10)
+    arr = [random.randint(0,100) for _ in range(20)]
+    print("原始数据：", arr)
+    select_sort(arr)
+    print("插入排序：", arr)
