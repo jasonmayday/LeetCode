0804b07c71646ef58350a2a4b59e7ff0fe4484bb
Jiexin Shi <jasonmayday@gmail.com>
2021-11-01 14:00:14 -0400

    Update 0145_二叉树的后序遍历.py

--- leetcode_solutions/0145_二叉树的后序遍历.py
+++ leetcode_solutions/0145_二叉树的后序遍历.py
@@ -30,7 +30,7 @@ class TreeNode:
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
-root.left.left =TreeNode(4)
+root.left.left = TreeNode(4)
 root.left.right = TreeNode(5)
 root.right.left = TreeNode(6)
 root.right.right = TreeNode(7)
