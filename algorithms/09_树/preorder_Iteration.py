bd575ad90f3ae50695398b2f21b78847819302c6
Jiexin Shi <jasonmayday@gmail.com>
2021-11-01 14:03:37 -0400

    Update preorder_Iteration.py

---
+++ algorithms/09_树/preorder_Iteration.py
@@ -0,0 +1,45 @@
+'''
+       1
+     ↙   ↘
+   2       3
+ ↙  ↘    ↙  ↘
+4    5   6    7
+                ↘
+                  8
+'''
+
+# 前序遍历：根结点 ---> 左子树 ---> 右子树
+
+# Definition for a binary tree node.
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left =TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+root.right.right.right = TreeNode(8)
+
+class Solution(object):
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def preorder(root):
+            if root:
+                res.append(root.val)
+                preorder(root.left)
+                preorder(root.right)
+        preorder(root)
+        return res
+
+sol = Solution()
+result = sol.preorderTraversal(root)
+print (result)  
