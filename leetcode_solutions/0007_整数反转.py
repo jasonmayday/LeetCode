cc11628ccf45dda134af27a71f94ac07455e11ef
Jiexin Shi <jasonmayday@gmail.com>
2021-11-01 17:07:11 -0400

    Create 0007_整数反转.py

---
+++ leetcode_solutions/0007_整数反转.py
@@ -0,0 +1,44 @@
+'''
+https://leetcode-cn.com/problems/reverse-integer/
+
+给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+
+如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+
+假设环境不允许存储 64 位整数（有符号或无符号）。
+
+示例 1：
+输入：x = 123
+输出：321
+
+示例 2：
+输入：x = -123
+输出：-321
+
+示例 3：
+输入：x = 120
+输出：21
+
+示例 4：
+输入：x = 0
+输出：0
+
+提示：
+-2^31 <= x <= 2^31-1
+
+'''
+
+
+def reverse_force(self, x: int) -> int:
+        if -10 < x < 10:
+            return x
+        str_x = str(x)
+        if str_x[0] != "-":
+            str_x = str_x[::-1]
+            x = int(str_x)
+        else:
+            str_x = str_x[:0:-1]
+            x = int(str_x)
+            x = -x
+        return x if -2147483648 < x < 2147483647 else 0
+
