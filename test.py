598fabe73000c75bd1f4465d6370ab860202acb9
Jiexin Shi <jasonmayday@gmail.com>
2021-11-02 18:26:20 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-class ListNode:
-    def __init__(self, val = 0, next=None):
-        self.val = val     # 保存当前节点的值
-        self.next = next   # 保存当前节点中下一个节点的引用
+str = "dog cat cat dog"
 
-l1 = ListNode(1)
-l1.next = ListNode(2)
-l1.next.next = ListNode(4)
+li = str.split(' ')
 
-l2 = ListNode(1)
-l2.next = ListNode(3)
-l2.next.next = ListNode(4)
-print(l1)
+print (li)
