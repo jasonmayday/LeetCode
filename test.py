c23d36d89bcd7ae17e5b4db98ca74ebd586ef765
Jiexin Shi <jasonmayday@gmail.com>
2021-11-02 00:35:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-strs = ["flower","flow","flight"]
+class ListNode:
+    def __init__(self, val = 0, next=None):
+        self.val = val     # 保存当前节点的值
+        self.next = next   # 保存当前节点中下一个节点的引用
 
-s = strs[0]
-i = 1
-print (s)
-print (str[i])
-print (strs[i].find(s))
-  
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+print(l1)
