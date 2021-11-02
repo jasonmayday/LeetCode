7ba3f2407a40d4a911b8d78249d82fa35d0413c5
Jiexin Shi <jasonmayday@gmail.com>
2021-11-02 16:06:54 -0400

    Update 0021_合并两个有序链表.py

--- leetcode_solutions/0021_合并两个有序链表.py
+++ leetcode_solutions/0021_合并两个有序链表.py
@@ -25,46 +25,14 @@ https://assets.leetcode.com/uploads/2020/10/03/merge_ex1.jpg
 l1 和 l2 均按 非递减顺序 排列
 
 '''
-# Definition for singly-linked list.
-class ListNode:   # 节点类
-    def __init__(self, val = 0, next = None):  # 初始化节点
-        self.val = val     # 保存当前节点的值
-        self.next = next   # 保存当前节点中下一个节点的引用
-        # 每个节点包含两个部分，一部分存放数据变量的data，另一部分存放下一个节点的位置信息next。
 
-class Linkedlist(): # 链表类
-    def __init__(self): # 初始化链表
-        self.head = None  # 头节点
-        self.length = 0   # 链表长度
-
-
-
-
-
-
-l1 = [1,2,4]
-l2 = [1,3,4]    
-
-node1 = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(4)
-node1.next = node2
-node2.next = node3
-
-# 递归
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if l1 is None:
-            return l2
-        elif l2 is None:
-            return l1
-        elif l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next,l2)
             return l1
         else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
+            l2.next = self.mergeTwoLists(l1,l2.next)
             return l2
-
-sol = Solution()
-result = sol.mergeTwoLists(l1, l2)
-print(result)
