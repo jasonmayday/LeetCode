f2dae634fb31f505acd92c589b653d3866ffde45
Jiexin Shi <jasonmayday@gmail.com>
2021-11-02 20:56:50 -0400

    Update 0028_实现strStr().py

--- leetcode_solutions/0028_实现strStr().py
+++ leetcode_solutions/0028_实现strStr().py
@@ -31,5 +31,51 @@ https://leetcode-cn.com/problems/implement-strstr/
 haystack = "hello"
 needle = "ll"
 
+# 解法1：遍历法
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if needle == '':
+            return 0
+        for i in range(0, len(haystack) - len(needle) + 1):  # 0-4范围内
+            if haystack[i:i+len(needle)] == needle:
+                return i
+            return -1
+
+# 解法2：调用find函数，str1.find(str2) 函数检测字符串str1中是否包含子字符串str2，如果包含子字符串返回开始的索引值，否则返回-1。
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+# 解法3：KMP算法
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        a=len(needle)
+        b=len(haystack)
+        if a==0:
+            return 0
+        next=self.getnext(a,needle)
+        p=-1
+        for j in range(b):
+            while p>=0 and needle[p+1]!=haystack[j]:
+                p=next[p]
+            if needle[p+1]==haystack[j]:
+                p+=1
+            if p==a-1:
+                return j-a+1
+        return -1
+
+    def getnext(self,a,needle):
+        next=['' for i in range(a)]
+        k=-1
+        next[0]=k
+        for i in range(1,len(needle)):
+            while (k>-1 and needle[k+1]!=needle[i]):
+                k=next[k]
+            if needle[k+1]==needle[i]:
+                k+=1
+            next[i]=k
+        return next
+
+sol = Solution()
+result = sol.strStr(haystack, needle)
+print(result)
