96b6140db9970316b3adff5a6f1f02d3f370960d
Jiexin Shi <jasonmayday@gmail.com>
2021-11-03 13:36:24 -0400

    Update 0160_相交链表.py

--- leetcode_solutions/0160_相交链表.py
+++ leetcode_solutions/0160_相交链表.py
@@ -57,17 +57,12 @@ class ListNode:
         self.next = None
 
 def create_linked_list(arr):
-    head_node = None
-    p_node = None
-    for a in arr:
-        new_node = ListNode(a)
-        if head_node is None:
-            head_node = new_node
-            p_node = new_node
-        else:
-            p_node.next = new_node
-            p_node = new_node
-    return head_node
+    head = ListNode(arr[0])
+    cur = head
+    for i in range(1, len(arr)):
+        cur.next = ListNode(arr[i])
+        cur = cur.next
+    return head
 
 def print_linked_list(head):
     cur = head
@@ -77,25 +72,24 @@ def print_linked_list(head):
         cur = cur.next
     return res
 
+# 解法1：双指针
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        # curr1指向链表A的头结点，curr2指向链表B的头结点
-        curr1, curr2 = headA, headB
-        while curr1 != curr2:
-            # curr1 = curr1.next if curr1 else headB
-            if curr1:
-                curr1 = curr1.next
+        cur1 = headA  # cur1指向链表A的头结点
+        cur2 = headB  # cur2指向链表B的头结点
+        # 让 cur1, cur2 指针都走一遍 headA, headB 两个链表, cur1, cur2 相遇的地方就是两个链表相交的地方.
+        while cur1 != cur2:
+            if cur1:
+                cur1 = cur1.next   # cur1 指向 headA, 一步一步往 next 走, 走到结尾 null 时, 跳到 headB 继续往后遍历
             else:
-                # 当 curr1 到达链表的尾部时，将它重定位到链表 B 的头结点
-                curr1 = headB
-            # curr2 = curr2.next if curr2 else headA
-            if curr2:
-                curr2 = curr2.next
+                cur1 = headB       # 当 curr1 到达链表的尾部时，将它重定位到链表 B 的头结点
+            
+            if cur2:
+                cur2 = cur2.next   # cur2 指向 headB, 一步一步往 next 走, 走到结尾 null 时, 跳到 headA 继续往后遍历
             else:
-                # 当 curr2 到达链表的尾部时，将它重定位到链表 A 的头结点。
-                curr2 = headA
+                cur2 = headA       # 当 curr2 到达链表的尾部时，将它重定位到链表 A 的头结点。
 
-        return curr1
+        return cur1
 """
 A和B两个链表长度可能不同，但是A+B和B+A的长度是相同的，所以遍历A+B和遍历B+A一定是同时结束。 
 如果A,B相交的话A和B有一段尾巴是相同的，所以两个遍历的指针一定会同时到达交点 如果A,B不相交的话两个指针就会同时到达A+B（B+A）的尾节点
