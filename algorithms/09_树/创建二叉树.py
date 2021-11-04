cef137d2f397c56fc422e8555ae2123c80f29628
Jiexin Shi <jasonmayday@gmail.com>
2021-11-04 14:04:21 -0400

    Create 创建二叉树.py

---
+++ algorithms/09_树/创建二叉树.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+    def insert(self, data):
+     # 将新值与父节点进行比较
+        if self.data:  # 非空
+            if data < self.data:            # 新值较小，放左边
+                if self.left is None:       # 若空，则新建插入节点
+                    self.left = TreeNode(data)
+                else:                       # 否则，递归往下查找
+                    self.left.insert(data)
+            elif data > self.data:          # 新值较大，放右边
+                if self.right is None:      # 若空，则新建插入节点
+                     self.right = TreeNode(data)
+                else:                       # 否则，递归往下查找
+                    self.right.insert(data)
+        else:
+            self.data = data                
+ 
+    # 打印这棵树，中序遍历
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+ 
+# 使用insert方法添加节点
+root = TreeNode(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+ 
+root.PrintTree()
