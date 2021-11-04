c6d588a82e8a2d35a09e8c267708fa345f7f917c
Jiexin Shi <jasonmayday@gmail.com>
2021-11-04 13:12:28 -0400

    Update 单链表实现栈.py

--- algorithms/05_链表/单链表实现栈.py
+++ algorithms/05_链表/单链表实现栈.py
@@ -3,66 +3,88 @@
     栈顶元素：栈顶元素即为链表的头结点
     压栈：向链表的头结点插进入栈元素，无表头链表则替换插入元素为头结点
     弹栈：弹出链表头结点，并将链表头结点替换为下一个元素
+    维护头指针和链表长度；每次压入元素则在头结点前插入新结点，取出元素则删除头结点。
 '''
-class LNode(object):
-    def __init__(self,x):
-        self.data = x
-        self.next = None
+class Empty(Exception):
+    """尝试对空栈进行删除操作时抛出的异常"""
+    pass
 
-class MyStack(object):
+
+class _Node:
+    """节点类"""
+
+    def __init__(self, element, next=None):
+        """
+        :param element: 节点代表的对象元素
+        :param next: 节点对象中用于指向下一个节点的实例属性
+        """
+        self.element = element
+        self.next = next
+
+
+class LinkedStack:
+    """使用单链表实现的栈"""
+
     def __init__(self):
-        #pHead = LNode
-        self.data = None
-        self.next = None
+        """创建一个空的栈"""
+        self._head = None  # 初始化头节点
+        self._size = 0  # 保存栈的元素数量
+
+    def __len__(self):
+        """
+        返回栈中当前元素数目
+        :return: 栈的元素数量
+        """
+        return self._size
 
     def is_empty(self):
-        """判断是否为空"""
-        if self.next == None:
-            return True
-        return False
-
-    def size(self):
-        """返回栈的大小"""
-        size=0
-        p = self.next
-        while p != None:
-        # while p is not None:
-            p = p.next
-            size += 1
-        return size
+        """
+        判断当前栈是否为空，如是则返回True
+        :return: 栈是否为空
+        """
+        return self._size == 0
 
     def push(self, element):
-        """压栈(加入元素)"""
-        p = LNode(element)
-        p.data = element
-        p.next = self.next
-        self.next = p
-
-    def pop(self):
-        """弹栈(弹出元素)"""
-        tmp = self.next
-        if tmp != None:
-            self.next = tmp.next
-        print("栈已经为空")
-        return None
+        """
+        :param element:
+        :return: None
+        """
+        node = _Node(element)  # 将元素封装进节点
+        node.next = self._head  # 先让新节点的next域指向原头节点
+        self._head = node  # 让新节点成为头节点
+        self._size += 1
 
     def top(self):
-        """返回栈顶元素"""
-        if self.next != None:
-            return self.next.data
-        print("栈已经为空")
-        return None
+        """
+        返回但不删除栈顶元素，当栈为空时抛出异常
+        :return: 栈顶元素
+        """
+        if self.is_empty():
+            raise Empty('栈为空！')
+        return self._head.element  # 栈顶元素即为单链表头部元素
 
-s = MyStack()
-s.push(1)
-print("栈顶元素为："+str(s.top()))
-print("栈大小为："+str(s.size()))
-s.pop()
-print("弹栈成功")
-s.pop()
-"""
-栈顶元素为：1
-栈大小为：1
-弹栈成功
-栈已经为空
-"""
+    def pop(self):
+        """
+        删除并返回栈顶元素，当栈为空时抛出异常
+        :return: 栈顶元素
+        """
+        if self.is_empty():
+            raise Empty('栈为空！')
+        ans = self._head.element
+        self._head = self._head.next  # 使当前头节点的下一个节点作为新的头节点
+        self._size -= 1
+        return ans
+
+
+def main():
+    stack = LinkedStack()
+    stack.push(5)
+    stack.push(3)
+    print(len(stack))        # 输出为：2
+    print(stack.pop())       # 输出为：3
+    print(stack.is_empty())  # 输出为：False
+    print(stack.pop())       # 输出为：5
+    print(stack.is_empty())  # 输出为：True
+
+if __name__ == '__main__':
+    main()
