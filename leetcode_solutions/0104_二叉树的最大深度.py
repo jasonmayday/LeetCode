fe7a6d697337e6eab03045001fe2c37ede582515
Jiexin Shi <jasonmayday@gmail.com>
2021-11-09 16:01:00 -0500

    Update 0104_二叉树的最大深度.py

--- leetcode_solutions/0104_二叉树的最大深度.py
+++ leetcode_solutions/0104_二叉树的最大深度.py
@@ -23,13 +23,34 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
+"""深度优先搜索"""
 class Solution:
     def maxDepth(self, root):
         if root is None: 
             return 0 
-        else: 
-            left_height = self.maxDepth(root.left) 
-            right_height = self.maxDepth(root.right) 
+        else:                                           # 如果我们知道了左子树和右子树的最大深度 ll 和 rr，那么该二叉树的最大深度即为 max(l,r)+1
+            left_height = self.maxDepth(root.left)      # 而左子树和右子树的最大深度又可以以同样的方式进行计算。
+            right_height = self.maxDepth(root.right)    # 先递归计算出其左子树和右子树的最大深度
             return max(left_height, right_height) + 1
             
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    root.left.left = TreeNode(3)
+    root.left.right = TreeNode(4)
+    root.right.left = TreeNode(4)
+    root.right.right = TreeNode(3)
+
+    sol = Solution()
+    result = sol.maxDepth(root)
+    print (result)  
+    '''
+        1
+       / \
+      2   2
+     / \ / \
+    3  4 4  3
+    ''' 
