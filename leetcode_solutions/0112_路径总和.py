1126de5e3c8e8feb47e119c76b255d9b2ecbd793
Jiexin Shi <jasonmayday@gmail.com>
2021-11-10 13:15:32 -0500

    Update 0112_路径总和.py

--- leetcode_solutions/0112_路径总和.py
+++ leetcode_solutions/0112_路径总和.py
@@ -45,7 +45,9 @@ class Tree:
 
         return level(0)
 
-"""BFS"""
+'''核心思想是对树进行一次遍历，在遍历时记录从根节点到当前节点的路径和，以防止重复计算。'''
+
+"""方法1：广度优先搜索"""
 class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root:
@@ -68,6 +70,16 @@ class Solution:
         return False
 
 """递归"""
+
+''' 观察要求我们完成的函数，我们可以归纳出它的功能：询问是否存在从当前节点 root 到叶子节点的路径，满足其路径和为 sum。
+
+    假定从根节点到当前节点的值之和为 val，我们可以将这个大问题转化为一个小问题：是否存在从当前节点的子节点到叶子的路径，满足其路径和为 sum - val。
+
+    不难发现这满足递归的性质，若当前节点就是叶子节点，那么我们直接判断 sum 是否等于 val 即可（因为路径和已经确定，就是当前节点的值，我们只需要判断该路径和是否满足条件）。
+    
+    若当前节点不是叶子节点，我们只需要递归地询问它的子节点是否能满足条件即可。
+
+'''
 class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root:
