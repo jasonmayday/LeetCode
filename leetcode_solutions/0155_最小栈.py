5cf593f25c8280d8bf8e89538d98427f081de4bd
Jiexin Shi <jasonmayday@gmail.com>
2021-11-10 20:16:55 -0500

    Update 0155_最小栈.py

--- leetcode_solutions/0155_最小栈.py
+++ leetcode_solutions/0155_最小栈.py
@@ -29,22 +29,28 @@ https://leetcode-cn.com/problems/min-stack/
     pop、top 和 getMin 操作总是在 非空栈 上调用。
 
 '''
+import math
 
 class MinStack:
     def __init__(self):
         self.stack = []
         self.min_stack = [math.inf]
 
+    # 将元素 x 推入栈中
     def push(self, x: int) -> None:
         self.stack.append(x)
         self.min_stack.append(min(x, self.min_stack[-1]))
-
+    
+    # 删除栈顶的元素
     def pop(self) -> None:
         self.stack.pop()
         self.min_stack.pop()
 
+    # 获取栈顶元素
     def top(self) -> int:
         return self.stack[-1]
 
+    # 检索栈中的最小元素
     def getMin(self) -> int:
         return self.min_stack[-1]
+
