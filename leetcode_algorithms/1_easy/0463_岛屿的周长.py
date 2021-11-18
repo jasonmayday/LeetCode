2e542a4dc7f2e3c065a7de75c1aa3bdc53913299
Jiexin Shi <jasonmayday@gmail.com>
2021-11-18 17:55:01 -0500

    Create 0463_岛屿的周长.py

---
+++ leetcode_algorithms/1_easy/0463_岛屿的周长.py
@@ -0,0 +1,68 @@
+"""
+https://leetcode-cn.com/problems/island-perimeter/
+
+给定一个 row x col 的二维网格地图 grid ，其中：grid[i][j] = 1 表示陆地， grid[i][j] = 0 表示水域。
+
+网格中的格子 水平和垂直 方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+
+岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+
+示例 1：
+    输入：grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+    输出：16
+    解释：它的周长是上面图片中的 16 个黄色的边
+
+示例 2：
+    输入：grid = [[1]]
+    输出：4
+
+示例 3：
+    输入：grid = [[1,0]]
+    输出：4
+
+提示：
+    row == grid.length
+    col == grid[i].length
+    1 <= row, col <= 100
+    grid[i][j] 为 0 或 1
+
+"""
+from typing import List
+
+
+'''解法1'''
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        length = len(grid)
+        width = len(grid[0])
+        res = 0
+        for i in range(length):
+            for j in range(width):
+                if grid[i][j] == 1:
+                    if j == 0 or grid[i][j - 1] == 0:
+                        res += 1
+                    if i == 0 or grid[i - 1][j] == 0:
+                        res += 1
+        return res * 2  # 由于岛屿内没有湖,所以只需要求出 北面(或南面) + 西面(或东面)的长度再乘2即可
+
+'''解法2'''
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        length = len(grid)
+        width = len(grid[0])
+        res = 0
+        for i in range(length):
+            for j in range(width):
+                if grid[i][j] == 1:     # 如果当前值为1： 加4（四条边）
+                    res += 4
+                    if i-1 >= 0 and grid[i-1][j] == 1:  # 如果左边有1，减2（两条边重合）
+                        res -= 2
+                    if j-1 >= 0 and grid[i][j-1] == 1:  # 如果上边有1，减2（两条边重合）
+                        res -= 2
+        return res
+
+if __name__ == "__main__":
+    grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+    sol = Solution()
+    result = sol.islandPerimeter(grid)
+    print(result)
