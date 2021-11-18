6cf135108ecd2cf1dc7c8664b49cf183eaf99a59
Jiexin Shi <jasonmayday@gmail.com>
2021-11-18 18:49:02 -0500

    Create 0459_重复的子字符串.py

---
+++ leetcode_algorithms/1_easy/0459_重复的子字符串.py
@@ -0,0 +1,44 @@
+"""
+给定一个非空的字符串，判断它是否可以由它的一个子串重复多次构成。给定的字符串只含有小写英文字母，并且长度不超过10000。
+
+示例 1:
+    输入: "abab"
+    输出: True
+    解释: 可由子字符串 "ab" 重复两次构成。
+
+示例 2:
+    输入: "aba"
+    输出: False
+
+示例 3:
+    输入: "abcabcabcabc"
+    输出: True
+    解释: 可由子字符串 "abc" 重复四次构成。 (或者子字符串 "abcabc" 重复两次构成。)
+
+"""
+
+'''枚举'''
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n = len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                if all(s[j] == s[j - i] for j in range(i, n)):
+                    return True
+        return False
+
+'''字符串匹配'''
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return (s + s).find(s, 1) != len(s)
+
+'''字符串匹配'''
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s+s)[1:-1] # 如果s中包含重复的子字符串，那么说明s中至少包含两个子字符串，s+s至少包含4个字串，前后各去掉一位，查找s是否在新构建的字符串中。
+
+if __name__ == "__main__":
+    s = "abcabcabcabc"
+    sol = Solution()
+    result = sol.repeatedSubstringPattern(s)
+    print(result)
