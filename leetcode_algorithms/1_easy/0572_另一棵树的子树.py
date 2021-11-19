779cd68d2f9ef33757aae8e2c94f43c93fad7404
Jiexin Shi <jasonmayday@gmail.com>
2021-11-19 14:42:44 -0500

    Update 0572_另一棵树的子树.py

--- leetcode_algorithms/1_easy/0572_另一棵树的子树.py
+++ leetcode_algorithms/1_easy/0572_另一棵树的子树.py
@@ -16,10 +16,57 @@ https://leetcode-cn.com/problems/subtree-of-another-tree/
 提示：
     root 树上的节点数量范围是 [1, 2000]
     subRoot 树上的节点数量范围是 [1, 1000]
-    -104 <= root.val <= 104
-    -104 <= subRoot.val <= 104
+    -10^4 <= root.val <= 10^4
+    -10^4 <= subRoot.val <= 10^4
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/subtree-of-another-tree
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.val)   
+
+"""递归构建"""
+def list_to_binarytree(nums):
+    def level(index):
+        if index >= len(nums) or nums[index] is None:
+            return None
+        root = TreeNode(nums[index])
+        root.left = level(2 * index + 1)    # 往左递推  # 从根开始一直到最左，直至为空
+        root.right = level(2 * index + 2)   # 往右回溯  # 再返回上一个根，回溯右
+        return root     # 再返回根
+    return level(0)
+
+class Solution(object):
+    def isSubtree(self, s, t):
+        if not s and not t:     # 两个 node 都为空为 True
+            return True
+        if not s or not t:      # 一方空，一方不空，为 False
+            return False
+        # 判断 t 是否为 s 的子树的三个条件是或的关系，即：
+        # 1. 当前两棵树相等；
+        # 2. 或者，t 是 s 的左子树；
+        # 3. 或者，t 是 s 的右子树。
+        return self.isSameTree(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+    def isSameTree(self, s, t):
+        if not s and not t:     # 两个 node 都为空为 True
+            return True
+        if not s or not t:      # 一方空，一方不空，为 False
+            return False
+        # 判断两个树是否相等的三个条件是与的关系，即：
+        # 1. 当前两个树的根节点值相等；
+        # 2. 并且，s 的左子树和 t 的左子树相等；
+        # 3. 并且，s 的右子树和 t 的右子树相等。
+        return s.val == t.val and self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+    
+if __name__ == "__main__":
+    s = list_to_binarytree([3,4,5,1,2])
+    t = list_to_binarytree([4,1,2])
+    sol = Solution()
+    result = sol.isSubtree(s,t)
+    print(result)
