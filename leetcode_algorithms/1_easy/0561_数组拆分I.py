7668e7df49fda173e30a6d2ffe3c63673c2b5f4c
Jiexin Shi <jasonmayday@gmail.com>
2021-11-19 12:56:00 -0500

    Update 0561_数组拆分I.py

--- leetcode_algorithms/1_easy/0561_数组拆分I.py
+++ leetcode_algorithms/1_easy/0561_数组拆分I.py
@@ -30,7 +30,7 @@ from typing import List
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
-        return sum(nums[::2])
+        return sum(nums[::2])   # [::2]命令：每两个位置输出一个字符
 
 if __name__ == "__main__":
     nums = [6,2,6,5,1,2]
