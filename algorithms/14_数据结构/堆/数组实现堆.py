ad8e7d69fef2556ef10e1263f17873b4cfdfde41
Jiexin Shi <jasonmayday@gmail.com>
2021-11-19 23:04:05 -0500

    Create 数组实现堆.py

---
+++ algorithms/14_数据结构/堆/数组实现堆.py
@@ -0,0 +1,105 @@
+class Heap:
+    def __init__(self,desc=False):
+        """
+        初始化，默认创建一个小顶堆
+        """
+        self.heap = []
+        self.desc = desc
+    
+    @property
+    def size(self):
+        return len(self.heap)
+    
+    def top(self):
+        if self.size:
+            return self.heap[0]
+        return None
+    
+    def push(self,item):
+        """
+        添加元素
+        第一步，把元素加入到数组末尾
+        第二步，把末尾元素向上调整
+        """
+        self.heap.append(item)
+        self._sift_up(self.size-1)
+    
+    def pop(self):
+        """
+        弹出堆顶
+        第一步，记录堆顶元素的值
+        第二步，交换堆顶元素与末尾元素
+        第三步，删除数组末尾元素
+        第四步，新的堆顶元素向下调整
+        第五步，返回答案
+        """
+        item = self.heap[0]
+        self._swap(0,self.size-1)
+        self.heap.pop()
+        self._sift_down(0)
+        return item
+    
+    def _smaller(self,lhs,rhs):
+        return lhs > rhs if self.desc else lhs < rhs
+    
+    def _sift_up(self,index):
+        """
+        向上调整
+        如果父节点和当前节点满足交换的关系
+        （对于小顶堆是父节点元素更大，对于大顶堆是父节点更小），
+        则持续将当前节点向上调整
+        """
+        while index:
+            parent = (index-1) // 2
+            
+            if self._smaller(self.heap[parent],self.heap[index]):
+                break
+                
+            self._swap(parent,index)
+            index = parent
+    
+    def _sift_down(self,index):
+        """
+        向下调整
+        如果子节点和当前节点满足交换的关系
+        （对于小顶堆是子节点元素更小，对于大顶堆是子节点更大），
+        则持续将当前节点向下调整
+        """
+        # 若存在子节点
+        while index*2+1 < self.size:
+            smallest = index
+            left = index*2+1
+            right = index*2+2
+            
+            if self._smaller(self.heap[left],self.heap[smallest]):
+                smallest = left
+                
+            if right < self.size and self._smaller(self.heap[right],self.heap[smallest]):
+                smallest = right
+                
+            if smallest == index:
+                break
+            
+            self._swap(index,smallest)
+            index = smallest
+    
+    def _swap(self,i,j):
+        self.heap[i],self.heap[j] = self.heap[j],self.heap[i]
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = Heap()
+        self.k = k
+        for num in nums:
+            self.heap.push(num)
+            if self.heap.size > k:
+                self.heap.pop()
+
+
+    def add(self, val: int) -> int:
+        self.heap.push(val)
+        if self.heap.size > self.k:
+            self.heap.pop()
+        return self.heap.top()
+
