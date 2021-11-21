fd7c9a686bad1fd9acb0a97f7b2cc2637732e38b
Jiexin Shi <jasonmayday@gmail.com>
2021-11-20 21:34:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-bits = [1, 2, 3, 4]
-print (len(bits))
+from collections import Counter
+
+licensePlate = "iMSlpe4"
+words = ["claim","consumer","student","camera","public","never","wonder","simple","thought","use"]
+    
+lic = [i.lower() for i in licensePlate if i.isalpha()]  # 只选取字母，且大写变成小写
+d = Counter(lic)
+
+words.sort(key = len)
+
+print (d)
