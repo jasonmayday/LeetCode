7e7ed2212d8fd6b81d15fff4bb2a4ba2ba504e79
Jiexin Shi <jasonmayday@gmail.com>
2021-11-21 13:01:41 -0500

    Create 0747_至少是其他数字两倍的最大数.py

---
+++ leetcode_algorithms/1_easy/0747_至少是其他数字两倍的最大数.py
@@ -0,0 +1,45 @@
+"""
+https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/
+
+给你一个整数数组 nums ，其中总是存在 唯一的 一个最大整数 。
+
+请你找出数组中的最大元素并检查它是否 至少是数组中每个其他数字的两倍 。如果是，则返回 最大元素的下标 ，否则返回 -1 。
+
+示例 1：
+    输入：nums = [3,6,1,0]
+    输出：1
+    解释：6 是最大的整数，对于数组中的其他整数，6 大于数组中其他元素的两倍。6 的下标是 1 ，所以返回 1 。
+
+示例 2：
+    输入：nums = [1,2,3,4]
+    输出：-1
+    解释：4 没有超过 3 的两倍大，所以返回 -1 。
+
+示例 3：
+    输入：nums = [1]
+    输出：0
+    解释：因为不存在其他数字，所以认为现有数字 1 至少是其他数字的两倍。
+
+提示：
+    1 <= nums.length <= 50
+    0 <= nums[i] <= 100
+    nums 中的最大元素是唯一的
+
+"""
+    
+class Solution:
+    def dominantIndex(self, nums):
+        if len(nums)==1:
+            return 0
+        sortednums = sorted(nums,reverse=True)  # 排序，反向，最大的在最前
+        if sortednums[0] >= sortednums[1] * 2:  # 如果最大的大于第二大的数字：
+            return nums.index(sortednums[0])    # 返回该最大数字在排序前数组的index
+        else:
+            return -1
+
+
+if __name__ == "__main__":
+    nums = [3,6,1,0]
+    sol = Solution()
+    result = sol.dominantIndex(nums)
+    print(result)
