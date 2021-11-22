35806823d19bc6bfba4a21fe02f61753b504dadf
Jiexin Shi <jasonmayday@gmail.com>
2021-11-22 00:20:10 -0500

    Create 0896_单调数列.py

---
+++ leetcode_algorithms/1_easy/0896_单调数列.py
@@ -0,0 +1,49 @@
+"""
+https://leetcode-cn.com/problems/monotonic-array/
+
+如果数组是单调递增或单调递减的，那么它是单调的。
+
+如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+
+当给定的数组 A 是单调数组时返回 true，否则返回 false。
+
+示例 1：
+    输入：[1,2,2,3]
+    输出：true
+
+示例 2：
+    输入：[6,5,4,4]
+    输出：true
+
+示例 3：
+    输入：[1,3,2]
+    输出：false
+
+示例 4：
+    输入：[1,2,4,5]
+    输出：true
+
+示例 5：
+    输入：[1,1,1]
+    输出：true
+
+提示：
+    1 <= A.length <= 50000
+    -100000 <= A[i] <= 100000
+
+"""
+class Solution:
+    def isMonotonic(self, A):
+        increase, decrease = False, False
+        for i in range(len(A)-1):
+            if A[i] < A[i+1]:
+                increase = True
+            elif A[i] > A[i+1]:
+                decrease = True
+        return not (increase and decrease)  # increase 和 decrease 不能同时存在
+
+if __name__ == "__main__":
+    A = [6,5,4,4]
+    sol = Solution()
+    result = sol.isMonotonic(A)
+    print(result)
