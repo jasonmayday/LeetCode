9d0415af087a3a8ad4f8503123566729a0871681
Jiexin Shi <jasonmayday@gmail.com>
2021-11-23 14:42:38 -0500

    Update 0476_数字的补数.py

--- leetcode_algorithms/1_easy/0476_数字的补数.py
+++ leetcode_algorithms/1_easy/0476_数字的补数.py
@@ -1,18 +1,20 @@
 '''
+https://leetcode-cn.com/problems/number-complement/
+
 给你一个 正 整数 num ，输出它的补数。补数是对该数的二进制表示取反。
 
 示例 1：
-输入：num = 5
-输出：2
-解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+    输入：num = 5
+    输出：2
+    解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
 
 示例 2：
-输入：num = 1
-输出：0
-解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
+    输入：num = 1
+    输出：0
+    解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
 
 '''
-num = 78
+
 class Solution:
     def findComplement(self, num: int) -> int:
         num = bin(num)[2:] # 转二进制
@@ -21,7 +23,9 @@ class Solution:
             if num[i] == '0':res += '1'
             else: res += '0'
         return int(res,2) # 转十进制
-        
-sol = Solution()
-result = sol.findComplement(num)
-print(result)
+
+if __name__ == "__main__":      
+    num = 78
+    sol = Solution()
+    result = sol.findComplement(num)
+    print(result)
