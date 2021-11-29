f4a0f4210a8aaf94bee5fd531794d4b272cfc92e
Jiexin Shi <jasonmayday@gmail.com>
2021-11-29 17:39:39 -0500

    Update 1137_第N个泰波那契数.py

--- leetcode_algorithms/1_easy/1137_第N个泰波那契数.py
+++ leetcode_algorithms/1_easy/1137_第N个泰波那契数.py
@@ -25,12 +25,28 @@ T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
 """
 import functools
 
+'''递归 + 记忆化搜索'''
 class Solution:
+    @functools.lru_cache()
     def tribonacci(self, n: int) -> int:
         if n == 0: return 0
         if n == 1: return 1
         if n == 2: return 1
         return self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+
+'''动态规划'''
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        
+        a, b, c = 0, 1, 1
+        for i in range(3, n + 1):
+            d = a + b + c
+            a, b, c = b, c, d
+        return d
 
 if __name__ == "__main__":
     n = 37
