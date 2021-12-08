f265ff041f4e912e98c3cf6764c083def2c35f22
Jiexin Shi <jasonmayday@gmail.com>
2021-12-08 00:18:51 -0500

    Create 1716_计算力扣银行的钱.py

---
+++ leetcode_algorithms/1_easy/1716_计算力扣银行的钱.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode-cn.com/problems/calculate-money-in-leetcode-bank/
+
+Hercy 想要为购买第一辆车存钱。他 每天 都往力扣银行里存钱。
+
+最开始，他在周一的时候存入 1 块钱。从周二到周日，他每天都比前一天多存入 1 块钱。在接下来每一个周一，他都会比 前一个周一 多存入 1 块钱。
+
+给你 n ，请你返回在第 n 天结束的时候他在力扣银行总共存了多少块钱。
+
+示例 1：
+    输入：n = 4
+    输出：10
+    解释：第 4 天后，总额为 1 + 2 + 3 + 4 = 10 。
+
+示例 2：
+    输入：n = 10
+    输出：37
+    解释：第 10 天后，总额为 (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37 。注意到第二个星期一，Hercy 存入 2 块钱。
+
+示例 3：
+    输入：n = 20
+    输出：96
+    解释：第 20 天后，总额为 (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96 。
+
+提示：
+    1 <= n <= 1000
+
+"""
+
+class Solution:
+    def totalMoney(self, n):
+        total = 0
+        week, day = divmod(n, 7)
+        # total += 28 * week + (week - 1) * week * 7 // 2 
+        total += week * (week + 7) * 7 // 2 # 合并后
+        total += (week + 1  + week + day ) * day // 2
+        return total
+
+if __name__ == "__main__":
+    n = 999
+    sol = Solution()
+    result = sol.totalMoney(n)
+    print(result)
