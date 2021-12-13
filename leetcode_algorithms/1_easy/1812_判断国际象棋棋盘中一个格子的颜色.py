0608a4b4253ac5d295bc4517a0b2323fabf89a33
Jiexin Shi <jasonmayday@gmail.com>
2021-12-13 12:58:42 -0500

    Update 1812_判断国际象棋棋盘中一个格子的颜色.py

--- leetcode_algorithms/1_easy/1812_判断国际象棋棋盘中一个格子的颜色.py
+++ leetcode_algorithms/1_easy/1812_判断国际象棋棋盘中一个格子的颜色.py
@@ -9,28 +9,22 @@ https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2021/04/03/chessboard.pn
 给定坐标一定代表国际象棋棋盘上一个存在的格子。坐标第一个字符是字母，第二个字符是数字。
 
 示例 1：
+    输入：coordinates = "a1"
+    输出：false
+    解释：如上图棋盘所示，"a1" 坐标的格子是黑色的，所以返回 false 。
 
-输入：coordinates = "a1"
-输出：false
-解释：如上图棋盘所示，"a1" 坐标的格子是黑色的，所以返回 false 。
 示例 2：
+    输入：coordinates = "h3"
+    输出：true
+    解释：如上图棋盘所示，"h3" 坐标的格子是白色的，所以返回 true 。
 
-输入：coordinates = "h3"
-输出：true
-解释：如上图棋盘所示，"h3" 坐标的格子是白色的，所以返回 true 。
 示例 3：
-
-输入：coordinates = "c7"
-输出：false
- 
+    输入：coordinates = "c7"
+    输出：false
 
 提示：
+    coordinates.length == 2
+    'a' <= coordinates[0] <= 'h'
+    '1' <= coordinates[1] <= '8'
 
-coordinates.length == 2
-'a' <= coordinates[0] <= 'h'
-'1' <= coordinates[1] <= '8'
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/determine-color-of-a-chessboard-square
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
