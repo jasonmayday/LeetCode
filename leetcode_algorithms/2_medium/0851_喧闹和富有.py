236484ef573becfff4fcc4b9abdfbf2bc5890716
jasonmayday <jasonmayday@gmail.com>
2021-12-14 19:55:07 -0500

    Update 0851_喧闹和富有.py

--- leetcode_algorithms/2_medium/0851_喧闹和富有.py
+++ leetcode_algorithms/2_medium/0851_喧闹和富有.py
@@ -37,3 +37,31 @@ https://leetcode-cn.com/problems/loud-and-rich/
     对 richer 的观察在逻辑上是一致的
 
 """
+from typing import List
+
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        n = len(quiet)
+        g = [[] for _ in range(n)]
+        for r in richer:
+            g[r[1]].append(r[0])
+
+        ans = [-1] * n
+        def dfs(x: int):
+            if ans[x] != -1:
+                return
+            ans[x] = x
+            for y in g[x]:
+                dfs(y)
+                if quiet[ans[y]] < quiet[ans[x]]:
+                    ans[x] = ans[y]
+        for i in range(n):
+            dfs(i)
+        return ans
+
+if __name__ == "__main__":
+    richer = [[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]]
+    quiet = [3,2,5,4,6,1,7,0]
+    sol = Solution()
+    result = sol.loudAndRich(richer, quiet)
+    print (result) 
