88c65c7ecb26a94552a3d7b8cb0335db40690bda
Jiexin Shi <jasonmayday@gmail.com>
2022-01-06 23:28:21 -0500

    Update LCP_22_黑白方格画.py

--- leetcode_algorithms_difficulty/1_easy/LCP_22_黑白方格画.py
+++ leetcode_algorithms_difficulty/1_easy/LCP_22_黑白方格画.py
@@ -30,3 +30,27 @@
 
 """
 
+class Solution:
+    def paintingPlan(self, n: int, k: int) -> int:
+        if k in (0,n*n):return 1
+        def get(n,a):
+            #计算组合公式迭代版
+            res = 1
+            for i in range(n,n-a,-1):
+                res*=i
+            for j in range(1,a+1):
+                res/=j
+            return res       
+        ans=0
+        for i in range(n):
+            for j in range(n):
+                if n*(i+j)-(i*j) ==k:
+                    ans+=get(n,i)*get(n,j)             
+        return int(ans)
+
+if __name__ == "__main__":
+    n = 2
+    k = 2
+    sol = Solution()
+    result = sol.paintingPlan(n, k)
+    print(result)
