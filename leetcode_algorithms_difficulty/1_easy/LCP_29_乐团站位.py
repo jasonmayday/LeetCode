008ed75cd83489aa2c68ffc7692fde4249702a55
Jiexin Shi <jasonmayday@gmail.com>
2022-01-06 23:28:58 -0500

    Update LCP_29_乐团站位.py

--- leetcode_algorithms_difficulty/1_easy/LCP_29_乐团站位.py
+++ leetcode_algorithms_difficulty/1_easy/LCP_29_乐团站位.py
@@ -39,7 +39,10 @@ class Solution:
 
         return 0
 
-作者：zesunlight
-链接：https://leetcode-cn.com/problems/SNJvJP/solution/zhu-bu-ding-wei-by-zesunlight-tio3/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+if __name__ == "__main__":
+    num = 3
+    Xpos = 0
+    Ypos = 2
+    sol = Solution()
+    result = sol.orchestraLayout(num, Xpos, Ypos)
+    print(result)
