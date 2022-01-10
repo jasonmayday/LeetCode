160ed4577bbe50b1b0c132964f6605e6d9e4e81d
Jiexin Shi <jasonmayday@gmail.com>
2022-01-10 00:39:28 -0500

    Update 2133_检查是否每一行每一列都包含全部整数.py

--- leetcode_algorithms_difficulty/1_easy/2133_检查是否每一行每一列都包含全部整数.py
+++ leetcode_algorithms_difficulty/1_easy/2133_检查是否每一行每一列都包含全部整数.py
@@ -31,27 +31,34 @@ https://leetcode-cn.com/problems/check-if-every-row-and-column-contains-all-numb
 """
 from typing import List
 
+"""解法1"""
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n = len(matrix)
-        occur = set()   # 每一行/列出现过的整数
-        # 判断每一行是否符合要求
+        occur = set()   # 用哈希集合统计每一行/列出现过的整数
+        
+        '''判断每一行是否符合要求，如 matrix[1][1], matrix[1][2], matrix[1][3]'''
         for i in range(n):
             occur.clear()   # 确保统计前哈希表为空
             for j in range(n):
-                if matrix[i][j] in occur:
-                    # 出现重复整数，该行不符合要求
+                if matrix[i][j] in occur:   # 出现重复整数，该行不符合要求
                     return False
                 occur.add(matrix[i][j])
-        # 判断每一列是否符合要求
+        
+        '''判断每一列是否符合要求，如 matrix[1][1], matrix[2][1], matrix[3][1]'''
         for i in range(n):
             occur.clear()   # 确保统计前哈希表为空
             for j in range(n):
-                if matrix[j][i] in occur:
-                    # 出现重复整数，该列不符合要求
+                if matrix[j][i] in occur:   # 出现重复整数，该列不符合要求
                     return False
                 occur.add(matrix[j][i])
         return True
+
+"""解法2"""
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        from itertools import chain
+        return all(set(list(range(1,len(matrix)+1)))==set(r) for r in chain(matrix,zip(*matrix)))
 
 if __name__ == "__main__":
     matrix = [[1,2,3], [3,1,2], [2,3,1]]
