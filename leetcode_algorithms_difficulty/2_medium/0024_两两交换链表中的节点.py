400722266c6335034ba7cf51ad360538347bbd37
Jiexin Shi <jasonmayday@gmail.com>
2022-01-12 00:26:27 -0500

    Update 0024_两两交换链表中的节点.py

--- leetcode_algorithms_difficulty/2_medium/0024_两两交换链表中的节点.py
+++ leetcode_algorithms_difficulty/2_medium/0024_两两交换链表中的节点.py
@@ -49,15 +49,44 @@ def printLinkedList(head):
     return list
 
 
-"""方法一：递归"""
+"""方法1：递归"""
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-        newHead = head.next
-        head.next = self.swapPairs(newHead.next)
-        newHead.next = head
+        if not head or not head.next:   # 递归的终止条件是链表中没有节点，或者链表中只有一个节点
+            return head                 # 假设链表是 1->2->3->4
+        newHead = head.next             # 新的链表的头节点 为 原始链表的第二个节点  先保存新的头节点 2
+        head.next = self.swapPairs(newHead.next)    # 继续递归，将其余节点进行两两交换，处理节点 3->4
+                                                    # 当递归结束返回后，就变成了 4->3
+		                                            # 于是head节点就指向了4，变成 1->4->3
+        newHead.next = head             # 新的链表的第二个节点 为 原始链表的头节点  将2节点指向1
         return newHead
+    
+"""方法2：栈"""
+class Solution(object):
+	def swapPairs(self, head):
+		if not (head and head.next):
+			return head
+		p = ListNode(-1)
+		# 用stack保存每次迭代的两个节点
+		# head指向新的p节点，函数结束时返回head.next即可
+		cur,head,stack = head,p,[]
+		while cur and cur.next:
+			# 将两个节点放入stack中
+			_,_ = stack.append(cur),stack.append(cur.next)
+			# 当前节点往前走两步
+			cur = cur.next.next
+			# 从stack中弹出两个节点，然后用p节点指向新弹出的两个节点
+			p.next = stack.pop()
+			p.next.next = stack.pop()
+			p = p.next.next
+		# 注意边界条件，当链表长度是奇数时，cur就不为空	
+		if cur:
+			p.next = cur
+		else:
+			p.next = None
+		return head.next
+
+
 
 if __name__ == "__main__":
     head = listToLinkedList([1,2,3,4])
