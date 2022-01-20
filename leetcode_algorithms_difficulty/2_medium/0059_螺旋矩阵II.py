e1d07a33340a124a575aaa940082be34a6e1524d
Jiexin Shi <jasonmayday@gmail.com>
2022-01-20 16:05:27 -0500

    Update 0059_螺旋矩阵II.py

--- leetcode_algorithms_difficulty/2_medium/0059_螺旋矩阵II.py
+++ leetcode_algorithms_difficulty/2_medium/0059_螺旋矩阵II.py
@@ -15,34 +15,46 @@ https://leetcode-cn.com/problems/spiral-matrix-ii/
     1 <= n <= 20
 
 """
+from typing import List
 
 class Solution:
-    def generateMatrix(self, n: int) -> [[int]]:
-        l, r, t, b = 0, n - 1, 0, n - 1
-        mat = [[0 for _ in range(n)] for _ in range(n)]
-        num, tar = 1, n * n
-        while num <= tar:
-            for i in range(l, r + 1): # left to right
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        l = 0       # 左边界
+        r = n - 1   # 右边界
+        t = 0       # 上边界
+        b = n - 1   # 下边界
+        mat = [[0 for _ in range(n)] for _ in range(n)]     # 生成一个 n×n 空矩阵 mat
+        num = 1
+        while num <= (n * n):
+            
+            '''从左向右模拟'''
+            for i in range(l, r + 1):
                 mat[t][i] = num
                 num += 1
-            t += 1
-            for i in range(t, b + 1): # top to bottom
+            t += 1      # 上边界 +1
+            
+            '''从上向下模拟'''
+            for i in range(t, b + 1):
                 mat[i][r] = num
                 num += 1
-            r -= 1
-            for i in range(r, l - 1, -1): # right to left
+            r -= 1      # 右边界 +1
+            
+            '''从右向左模拟'''
+            for i in range(r, l - 1, -1):
                 mat[b][i] = num
                 num += 1
-            b -= 1
-            for i in range(b, t - 1, -1): # bottom to top
+            b -= 1      # 下边界 +1
+            
+            '''从下向上模拟'''
+            for i in range(b, t - 1, -1):
                 mat[i][l] = num
                 num += 1
-            l += 1
+            l += 1      # 左边界 +1
+        
         return mat
 
 if __name__ == "__main__":
-    candidates = [2,3,6,7]
-    target = 7
+    n = 4
     sol = Solution()
-    result = sol.combinationSum(candidates, target)
+    result = sol.generateMatrix(n)
     print(result)
