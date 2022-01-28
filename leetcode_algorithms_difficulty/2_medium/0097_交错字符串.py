2711ffc7317066e3425f342062c8eed7c678dc56
Jiexin Shi <jasonmayday@gmail.com>
2022-01-27 20:30:34 -0500

    Update 0097_交错字符串.py

--- leetcode_algorithms_difficulty/2_medium/0097_交错字符串.py
+++ leetcode_algorithms_difficulty/2_medium/0097_交错字符串.py
@@ -29,3 +29,39 @@ https://leetcode-cn.com/problems/interleaving-string/
     s1、s2、和 s3 都由小写英文字母组成
 
 """
+
+""" 使用 dp[i][j] 表示 s1 的前 i 个字符和 s2 的前 j 个字符是否能构成 s3 的前 i+j 个字符。"""
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        len1 = len(s1)  # aabcc
+        len2 = len(s2)  # dbbca
+        len3 = len(s3)  # aadbbcbcac
+        if(len1 + len2 != len3):
+            return False
+        dp = [[False]*(len2+1) for i in range(len1+1)]  # 初始化 dp 为 (len1+1) ∗ (len2+1) 的 False 数组
+        '''
+            "   d   b   b   c   a
+        "   F   F   F   F   F   F
+        a   F   F   F   F   F   F
+        a   F   F   F   F   F   F
+        b   F   F   F   F   F   F
+        c   F   F   F   F   F   F
+        c   F   F   F   F   F   F
+        '''
+        dp[0][0]=True
+        for i in range(1,len1+1):
+            dp[i][0]=(dp[i-1][0] and s1[i-1]==s3[i-1])
+        for i in range(1,len2+1):
+            dp[0][i]=(dp[0][i-1] and s2[i-1]==s3[i-1])
+        for i in range(1,len1+1):
+            for j in range(1,len2+1):
+                dp[i][j]=(dp[i][j-1] and s2[j-1]==s3[i+j-1]) or (dp[i-1][j] and s1[i-1]==s3[i+j-1])
+        return dp[-1][-1]
+
+if __name__ == "__main__":
+    s1 = "aabcc"
+    s2 = "dbbca"
+    s3 = "aadbbcbcac"
+    sol = Solution()
+    result = sol.isInterleave(s1,s2,s3)
+    print(result)
