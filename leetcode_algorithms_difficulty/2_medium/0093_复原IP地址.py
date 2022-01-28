1b8e9b580554a1e6423df681f3b0c0cb8570f866
Jiexin Shi <jasonmayday@gmail.com>
2022-01-27 23:39:00 -0500

    Create 0093_复原IP地址.py

---
+++ leetcode_algorithms_difficulty/2_medium/0093_复原IP地址.py
@@ -0,0 +1,78 @@
+"""
+https://leetcode-cn.com/problems/restore-ip-addresses/
+
+有效 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+
+    例如："0.1.2.201" 和 "192.168.1.1" 是 有效 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效 IP 地址。
+
+给定一个只包含数字的字符串 s ，用以表示一个 IP 地址，返回所有可能的有效 IP 地址，这些地址可以通过在 s 中插入 '.' 来形成。你不能重新排序或删除 s 中的任何数字。你可以按 任何 顺序返回答案。
+
+示例 1：
+    输入：s = "25525511135"
+    输出：["255.255.11.135","255.255.111.35"]
+
+示例 2：
+    输入：s = "0000"
+    输出：["0.0.0.0"]
+
+示例 3：
+    输入：s = "1111"
+    输出：["1.1.1.1"]
+
+示例 4：
+    输入：s = "010010"
+    输出：["0.10.0.10","0.100.1.0"]
+
+示例 5：
+    输入：s = "101023"
+    输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+
+提示：
+    0 <= s.length <= 20
+    s 仅由数字组成
+
+"""
+from typing import List
+
+""" 回溯 """
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        SEG_COUNT = 4
+        ans = list()
+        segments = [0] * SEG_COUNT
+        
+        def dfs(segId: int, segStart: int):
+            # 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
+            if segId == SEG_COUNT:
+                if segStart == len(s):
+                    ipAddr = ".".join(str(seg) for seg in segments)
+                    ans.append(ipAddr)
+                return
+            
+            # 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+            if segStart == len(s):
+                return
+
+            # 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+            if s[segStart] == "0":
+                segments[segId] = 0
+                dfs(segId + 1, segStart + 1)
+            
+            # 一般情况，枚举每一种可能性并递归
+            addr = 0
+            for segEnd in range(segStart, len(s)):
+                addr = addr * 10 + (ord(s[segEnd]) - ord("0"))
+                if 0 < addr <= 0xFF:
+                    segments[segId] = addr
+                    dfs(segId + 1, segEnd + 1)
+                else:
+                    break
+        
+        dfs(0, 0)
+        return ans
+
+if __name__ == "__main__":
+    s = "101023"
+    sol = Solution()
+    result = sol.restoreIpAddresses(s)
+    print(result)
