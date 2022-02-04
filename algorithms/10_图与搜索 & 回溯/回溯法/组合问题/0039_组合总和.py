4b2b8b276ca60201e9b18c7b5596e9d6746c38cf
Jiexin Shi <jasonmayday@gmail.com>
2022-02-03 20:15:55 -0500

    Update 0039_组合总和.py

--- algorithms/10_图与搜索 & 回溯/回溯法/组合问题/0039_组合总和.py
+++ algorithms/10_图与搜索 & 回溯/回溯法/组合问题/0039_组合总和.py
@@ -1,5 +1,6 @@
 """
 https://leetcode-cn.com/problems/combination-sum/
+
 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
 
 candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
@@ -42,7 +43,6 @@ from collections import defaultdict
 
 """ 回溯算法
     以 target = 7 为 根结点 ，创建一个分支的时 做减法"""
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 
