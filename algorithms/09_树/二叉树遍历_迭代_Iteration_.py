9d1d1e78ba929b4cc552eb91ae3a5db5721eab9c
Jiexin Shi <jasonmayday@gmail.com>
2022-02-10 14:00:43 -0500

    Update 二叉树遍历_迭代_Iteration_.py

--- algorithms/09_树/二叉树遍历_迭代_Iteration_.py
+++ algorithms/09_树/二叉树遍历_迭代_Iteration_.py
@@ -134,22 +134,22 @@ class Solution(object):
         return res
 
 if __name__ == "__main__":
-    root = TreeNode(1)
+    root = TreeNode(4)
     root.left = TreeNode(2)
-    root.right = TreeNode(3)
-    root.left.left = TreeNode(4)
-    root.left.right = TreeNode(5)
-    root.right.left = TreeNode(6)
+    root.right = TreeNode(6)
+    root.left.left = TreeNode(1)
+    root.left.right = TreeNode(3)
+    root.right.left = TreeNode(5)
     root.right.right = TreeNode(7)
     root.right.right.right = TreeNode(8)
     '''
-           1
+           4
          ↙   ↘
-       2       3
+       2       6
      ↙  ↘    ↙  ↘
-    4    5   6    7
-                    ↘
-                      8
+    1    3   5    7
+                     ↘
+                       8
     '''
 
     sol = Solution()
