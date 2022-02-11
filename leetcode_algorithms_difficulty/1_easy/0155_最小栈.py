22cc2dbb7a8fc56a02f4bdad54107d896e9c781f
Jiexin Shi <jasonmayday@gmail.com>
2022-02-11 13:09:48 -0500

    Update 0155_最小栈.py

--- leetcode_algorithms_difficulty/1_easy/0155_最小栈.py
+++ leetcode_algorithms_difficulty/1_easy/0155_最小栈.py
@@ -9,21 +9,21 @@ https://leetcode-cn.com/problems/min-stack/
  
 示例:
     输入：
-    ["MinStack","push","push","push","getMin","pop","top","getMin"]
-    [[],[-2],[0],[-3],[],[],[],[]]
+        ["MinStack","push","push","push","getMin","pop","top","getMin"]
+        [[],[-2],[0],[-3],[],[],[],[]]
 
     输出：
-    [null,null,null,null,-3,null,0,-2]
+        [null,null,null,null,-3,null,0,-2]
 
     解释：
-    MinStack minStack = new MinStack();
-    minStack.push(-2);
-    minStack.push(0);
-    minStack.push(-3);
-    minStack.getMin();   --> 返回 -3.
-    minStack.pop();
-    minStack.top();      --> 返回 0.
-    minStack.getMin();   --> 返回 -2.
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin();   --> 返回 -3.
+        minStack.pop();
+        minStack.top();      --> 返回 0.
+        minStack.getMin();   --> 返回 -2.
  
 提示：
     pop、top 和 getMin 操作总是在 非空栈 上调用。
@@ -31,6 +31,8 @@ https://leetcode-cn.com/problems/min-stack/
 '''
 import math
 
+""" 本题难点：获取栈最小值 min() 函数需要遍历整个栈，复杂度为 O(N) 。
+    借助辅助栈实现："""
 class MinStack:
     def __init__(self):
         self.stack = []
@@ -53,4 +55,34 @@ class MinStack:
     # 检索栈中的最小元素
     def getMin(self) -> int:
         return self.min_stack[-1]
+    
+""" 解法2 """
+class MinStack:
+    def __init__(self):
+        self.A, self.B = [], []
 
+    def push(self, x: int) -> None:
+        self.A.append(x)                    # 执行「元素 x 压入栈 A」 ；
+        if not self.B or self.B[-1] >= x:   # 保持栈 B 的元素是 非严格降序 的；
+            self.B.append(x)                # 若「栈 B 为空」或「x ≤ 栈 B 的栈顶元素」，则执行「元素 x 压入栈 B」 ；
+
+    def pop(self) -> None:
+        if self.A.pop() == self.B[-1]:  # 执行「栈 A 元素出栈」，将出栈元素记为 y ；
+            self.B.pop()                # 若 「y 等于栈 B 的栈顶元素」，则执行「栈 B 元素出栈」；
+
+    def top(self) -> int:
+        return self.A[-1]
+
+    def getMin(self) -> int:
+        return self.B[-1]
+
+
+if __name__ == "__main__":
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    print(minStack.getMin())   # 返回 -3
+    minStack.pop()
+    print(minStack.top())      # 返回  0
+    print(minStack.getMin())   # 返回 -2
