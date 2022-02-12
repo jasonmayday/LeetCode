e275cc31b7890bde1844a0389776012e9bae7ac1
Jiexin Shi <jasonmayday@gmail.com>
2022-02-12 18:26:51 -0500

    Update 0231_2的幂.py

--- leetcode_algorithm/1_easy/0231_2的幂.py
+++ leetcode_algorithm/1_easy/0231_2的幂.py
@@ -6,39 +6,37 @@ https://leetcode-cn.com/problems/power-of-two/
 如果存在一个整数 x 使得 n == 2^x ，则认为 n 是 2 的幂次方。
 
 示例 1：
-输入：n = 1
-输出：true
-解释：2^0 = 1
+    输入：n = 1
+    输出：true
+    解释：2^0 = 1
 
 示例 2：
-输入：n = 16
-输出：true
-解释：2^4 = 16
+    输入：n = 16
+    输出：true
+    解释：2^4 = 16
 
 示例 3：
-输入：n = 3
-输出：false
+    输入：n = 3
+    输出：false
 
 示例 4：
-输入：n = 4
-输出：true
+    输入：n = 4
+    输出：true
 
 示例 5：
-输入：n = 5
-输出：false
+    输入：n = 5
+    输出：false
 
 提示：
--2 ^ 31 <= n <= 2 ^ 31 - 1
+    -2 ^ 31 <= n <= 2 ^ 31 - 1
 
 '''
-# 在题目给定的 32 位有符号整数的范围内，最大的 2 的幂为 2 ^ 30 = 10737418242。
-# 我们只需要判断 n 是否是 2^30 的约数即可。
 
-
+""" 判断是否是最大幂的约数 """
 class Solution:
-    BIG = 2 ** 30
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and Solution.BIG % n == 0
+        biggest = 2 ** 30                   # 在题目给定的 32 位有符号整数的范围内，最大的 2 的幂为 2 ^ 30 = 10737418242。
+        return n > 0 and biggest % n == 0   # 只需要判断 n 是否是 2^30 的约数即可
 
 if __name__ == "__main__":
     n = 512
