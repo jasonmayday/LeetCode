8bc027e8d7ea547d74f9ed2b2c24eba6ff39eb45
Jiexin Shi <jasonmayday@gmail.com>
2022-02-12 16:15:40 -0500

    Update 0201_数字范围按位与.py

--- leetcode_algorithm/2_medium/0201_数字范围按位与.py
+++ leetcode_algorithm/2_medium/0201_数字范围按位与.py
@@ -16,7 +16,29 @@ https://leetcode-cn.com/problems/bitwise-and-of-numbers-range/
     输出：0
 
 提示：
-    0 <= left <= right <= 231 - 1
+    0 <= left <= right <= 2^31 - 1
 
 """
 
+""" 位移 + 公共前缀
+    规律：对所有数字执行按位与运算的结果是所有对应二进制字符串的公共前缀再用零补上后面的剩余位。 """
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+        while left < right:
+            left = left >> 1    # 将二进制不断做右移，
+            right = right >> 1  # 当 left 和 right 相同时停止，这样可以找到二进制的公共前缀
+            shift += 1          # 每右移一位，位移次数计数 +1
+        return right << shift   # 再把变化后的 left 或者 right 左移 之前 右移过的步数
+"""
+9   0000 1001
+10  0000 1010
+11  0000 1011
+12  0000 1100
+"""
+if __name__ == "__main__":
+    left = 9
+    right = 12
+    sol = Solution()
+    result = sol.rangeBitwiseAnd(left, right)
+    print (result)
