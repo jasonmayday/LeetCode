805eca69fcb58d76c9b587c2aed96f74183b83d9
Jiexin Shi <jasonmayday@gmail.com>
2022-02-12 21:00:53 -0500

    Update 0371_两整数之和.py

--- leetcode_algorithm/2_medium/0371_两整数之和.py
+++ leetcode_algorithm/2_medium/0371_两整数之和.py
@@ -15,3 +15,28 @@ https://leetcode-cn.com/problems/sum-of-two-integers/
     -1000 <= a, b <= 1000
 
 """
+
+"""方法一：位运算"""
+MASK1 = 4294967296  # 2^32
+MASK2 = 2147483648  # 2^31
+MASK3 = 2147483647  # 2^31-1
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        a %= MASK1
+        b %= MASK1
+        while b != 0:
+            carry = ((a & b) << 1) % MASK1
+            a = (a ^ b) % MASK1
+            b = carry
+        if a & MASK2:   # 负数
+            return ~((a ^ MASK2) ^ MASK3)
+        else:           # 正数
+            return a
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    sol = Solution()
+    result = sol.getSum(a, b)
+    print (result) 
