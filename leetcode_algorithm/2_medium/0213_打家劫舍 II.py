88d864fa10e9a74dab7ab371b64ae4b08926f9af
Jiexin Shi <jasonmayday@gmail.com>
2022-02-17 00:12:23 -0500

    Update 0213_打家劫舍 II.py

--- leetcode_algorithm/2_medium/0213_打家劫舍 II.py
+++ leetcode_algorithm/2_medium/0213_打家劫舍 II.py
@@ -26,38 +26,23 @@
 
 from typing import List
 
+""" 动态规划 """
 class Solution(object):
     def rob(self, nums: List[int]) -> int:
-        #   那么，很明显可以分为三种情况：
-        #   1. 首位都不偷
-        #   2. 偷首不偷尾
-        #   3. 不偷首偷尾
-        # 显然，第1种方式损失太大，选取2、3。
-        # 那么，分为两种情况，分别计算不包含首和不包含尾这两种情况来判断哪个大哪个小
-
-        # 1.dp[i] 代表当前最大子序和
-        # 2.动态方程: dp[i] = max(dp[i-1] and , nums[i-1]+dp[i-2])
-        # 3.初始化: 给没有房子时，dp一个位置，即：dp[0]
-        #   3.1 当size=0时，没有房子，dp[0]=0；
-        #   3.2 当size=1时，有一间房子，偷即可：dp[1]=nums[0]
-
-        # 依照《打家劫舍I》的优化方案进行计算
-
-        # nums处理，分别切割出去首和去尾的子串
-        nums1 = nums[1:]
-        nums2 = nums[:-1]
+        nums1 = nums[1:]    # 不偷第一个房子
+        nums2 = nums[:-1]   # 不偷最后一个房子
 
         size = len(nums)
-        if size == 0:
+        if size == 0:   # 没有房子，dp[0]=0
             return 0
-        if size == 1:
+        if size == 1:   # 有一间房子，偷即可
             return nums[0]
 
         def handle(size, nums):
             dp1 = 0
-            dp2 = nums[0]
-            for i in range(2, size+1):
-                dp1 = max(dp2, nums[i-1]+dp1)
+            dp2 = nums[0]   # dp[i] 代表当前最大子序和
+            for i in range(2, size + 1):
+                dp1 = max(dp2, nums[i-1] + dp1)   # 状态转移方程
                 dp1, dp2 = dp2, dp1
             return dp2
 
@@ -65,6 +50,18 @@ class Solution(object):
         res2 = handle(size-1, nums2)
 
         return max(res1, res2)
+
+
+""" 动态规划 """
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def my_rob(nums):
+            cur, pre = 0, 0
+            for num in nums:
+                cur, pre = max(pre + num, cur), cur
+            return cur
+        return max(my_rob(nums[:-1]),my_rob(nums[1:])) if len(nums) != 1 else nums[0]
+
 
 if __name__ == "__main__":
     nums = [1,2,3,1]
