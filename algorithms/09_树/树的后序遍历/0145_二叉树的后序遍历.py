ef27eb4b39caa3eb9b403b49533cb1a8b8f8e04c
Jiexin Shi <jasonmayday@gmail.com>
2022-02-17 11:29:38 -0500

    Update 0145_二叉树的后序遍历.py

--- algorithms/09_树/树的后序遍历/0145_二叉树的后序遍历.py
+++ algorithms/09_树/树的后序遍历/0145_二叉树的后序遍历.py
@@ -19,31 +19,13 @@
 
 from typing import List
 
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-root.right.left = TreeNode(6)
-root.right.right = TreeNode(7)
-root.right.right.right = TreeNode(8)
-'''
-       1
-     ↙   ↘
-   2       3
- ↙  ↘    ↙  ↘
-4    5   6    7
-                ↘
-                  8
-'''
-# 递归
+""" 递归 """
 class Solution(object):
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
@@ -55,7 +37,8 @@ class Solution(object):
         postorder(root)
         return res
 
-# 迭代    
+""" 迭代 """
+class Solution(object):
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         res,stack = [],[]
         while stack or root: 
@@ -68,6 +51,25 @@ class Solution(object):
         return res[::-1]
 
 if __name__ == "__main__":
+    
+    '''
+           1
+         ↙   ↘
+       2       3
+     ↙  ↘    ↙  ↘
+    4    5   6    7
+                    ↘
+                      8
+    '''
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    root.right.right.right = TreeNode(8)
+
     sol = Solution()
     result = sol.postorderTraversal(root)
     print (result)  
