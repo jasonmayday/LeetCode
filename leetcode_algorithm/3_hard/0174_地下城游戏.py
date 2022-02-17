803a3d8fe7dd924de670283dae59bf2f65b1c510
Jiexin Shi <jasonmayday@gmail.com>
2022-02-17 00:13:24 -0500

    Update 0174_地下城游戏.py

--- leetcode_algorithm/3_hard/0174_地下城游戏.py
+++ leetcode_algorithm/3_hard/0174_地下城游戏.py
@@ -23,3 +23,58 @@ https://leetcode-cn.com/problems/dungeon-game/
     任何房间都可能对骑士的健康点数造成威胁，也可能增加骑士的健康点数，包括骑士进入的左上角房间以及公主被监禁的右下角房间。
 
 """
+from typing import List
+
+""" 动态规划 (从右下往左上进行动态规划) """
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        r = len(dungeon)    # 行数
+        c = len(dungeon[0]) # 列数
+        BIG = 10 ** 9
+        dp = [[BIG] * (c + 1) for _ in range(r + 1)]
+        # 从右下往左上进行动态规划，令 dp[i][j] 表示从坐标 (i,j) 到终点所需的最小初始值
+        dp[r][c - 1] = dp[r - 1][c] = 1 # 边界条件
+        for i in range(r - 1, -1, -1):
+            for j in range(c - 1, -1, -1):
+                minn = min(dp[i + 1][j], dp[i][j + 1])
+                dp[i][j] = max(minn - dungeon[i][j], 1)
+
+        return dp[0][0]
+
+
+""" 动态规划 (从右下往左上进行动态规划) """
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        x = len(dungeon)
+        y = len(dungeon[0])
+        dp = [[None for __ in range(y)] for __ in range(x)]
+        
+        # 先填充最后一格
+        dp[-1][-1] = 1 if dungeon[-1][-1] >= 0 else -dungeon[-1][-1]+1
+        
+        # 填充最后一列
+        for i in range(x-2, -1, -1):
+            tmp = dp[i+1][-1]-dungeon[i][-1]
+            dp[i][-1] = 1 if tmp <= 0 else tmp
+        
+        # 填充最后一行
+        for i in range(y-2, -1, -1):
+            tmp = dp[-1][i+1]-dungeon[-1][i]
+            dp[-1][i] = 1 if tmp <= 0 else tmp
+        
+        # 填充其他
+        for i in range(x-2, -1, -1):
+            for j in range(y-2, -1, -1):
+                tmp = min(dp[i][j+1], dp[i+1][j])-dungeon[i][j]
+                dp[i][j] = 1 if tmp <= 0 else tmp
+
+        return dp[0][0]
+
+
+if __name__ == "__main__":
+    dungeon =  [[-2,  -3,  3],
+                [-5, -10,  1],
+                [10,  30, -5]]
+    sol = Solution()
+    result = sol.calculateMinimumHP(dungeon)
+    print(result)
