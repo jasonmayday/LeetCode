739cfdefbd353e4d80bf3cae7cada956092d6d3c
Jiexin Shi <jasonmayday@gmail.com>
2022-02-21 23:33:41 -0500

    Update 2182_构造限制重复的字符串.py

--- leetcode_algorithm/2_medium/2182_构造限制重复的字符串.py
+++ leetcode_algorithm/2_medium/2182_构造限制重复的字符串.py
@@ -34,32 +34,25 @@ https://leetcode-cn.com/problems/construct-string-with-repeat-limit/
     s 由小写英文字母组成
 
 """
-from collections import Counter
 
+
+""" 方法一：双指针 """
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        freq = Counter(s)
-
-        ans = list()
-        for c in range(25, -1, -1):
-            ch = chr(ord("a") + c)
-            ans.extend([ch] * freq[ch])
-            
+        ans = sorted (list(s), reverse = True)  # 字符串转为列表，然后根据字母表倒序排列    ['z', 'z', 'c', 'c', 'c', 'c', 'a']
         cnt = 1
-        # 双指针
         right = 2
         for left in range(1, len(ans)):
-            if ans[left - 1] != ans[left]:
-                cnt = 1
-            else:
-                cnt += 1
-                if cnt > repeatLimit:
-                    right = max(right, left + 1)
-                    while right < len(ans) and ans[left] == ans[right]:
-                        right += 1
-                    
-                    if right < len(ans):
-                        ans[left], ans[right] = ans[right], ans[left]
+            if ans[left - 1] != ans[left]:  # 左指针与左指针左边的不一样
+                cnt = 1                     # cnt 重置为 1
+            else:                           # 左指针与左指针左边的一样
+                cnt += 1                    # 统计有多少个连续相同的
+                if cnt > repeatLimit:       # 如果连续出现的字符超过限制：
+                    right = max(right, left + 1)                        # 生成右指针
+                    while right < len(ans) and ans[left] == ans[right]: # 在数组范围内，如果连续相同的字符继续重复：
+                        right += 1                                      # 右指针继续向右移，直到右指针遇到和左指针不同的字符
+                    if right < len(ans):                                # 在数组范围内
+                        ans[left], ans[right] = ans[right], ans[left]   # 左右指针的数字交换，（第 "repeatLimit+1 个重复字符" 和 "后面第一个与之前不重复的字符" 交换）
                         cnt = 1
                     else:
                         ans = ans[:left]
