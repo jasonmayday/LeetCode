a8ee9e206f60f0d675bab313cbeeb206dabc0b51
Jiexin Shi <jasonmayday@gmail.com>
2022-02-25 10:39:37 -0500

    Update 0270_最接近的二叉搜索树值.py

--- leetcode_algorithm/1_easy/0270_最接近的二叉搜索树值.py
+++ leetcode_algorithm/1_easy/0270_最接近的二叉搜索树值.py
@@ -20,3 +20,53 @@ https://leetcode-cn.com/problems/closest-binary-search-tree-value/
     输出: 4
 
 """
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+""" 方法一：递归 + 线性搜索"""
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        def inorder(r: TreeNode):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        return min(inorder(root), key = lambda x: abs(target - x))
+
+
+"""方法二：迭代"""
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        stack, pred = [], float('-inf')
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            
+            if pred <= target and target < root.val:
+                return min(pred, root.val, key = lambda x: abs(target - x))
+
+            pred = root.val
+            root = root.right
+
+        return pred
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+
+    sol = Solution()
+    result = sol.minDepth(root)
+    print (result)  
+    '''
+        3
+       / \
+      9   20
+         / \
+        15  7
+    ''' 
