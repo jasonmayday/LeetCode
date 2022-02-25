e2d1b6ba7d4c13c2ad98537a321709751ab64cb0
Jiexin Shi <jasonmayday@gmail.com>
2022-02-24 21:18:32 -0500

    Update 1213_三个有序数组的交集.py

--- leetcode_algorithm/1_easy/1213_三个有序数组的交集.py
+++ leetcode_algorithm/1_easy/1213_三个有序数组的交集.py
@@ -17,3 +17,54 @@ https://leetcode-cn.com/problems/intersection-of-three-sorted-arrays/
     1 <= arr1[i], arr2[i], arr3[i] <= 2000
 
 """
+from typing import List
+
+class Solution():
+    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+        res = []
+        count = [0 for _ in range(2005)]
+        for num in arr1 + arr2 + arr3:
+            count[num] += 1
+        for i in range(len(count)):
+            if count[i] == 3:
+                res.append(i)
+        return res
+
+
+class Solution():
+    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+        return sorted(list(set(arr1) & set(arr2) & set(arr3)))
+
+
+""" 三指针 """
+class Solution:
+    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+        i,j,k = 0,0,0       # 初始化三指针
+        minlength = min([len(arr1),len(arr2),len(arr3)])    # 获取最小长度
+        ans = []
+        while i < minlength and j < minlength and k < minlength:  # 只要有一个数组遍历结束，整体查找即告结束
+            if arr1[i] == arr2[j] == arr3[k]:   # 此轮遍历三个数组的值相同，即找到了同时出现在三个数组中的值
+                ans.append(arr1[i])
+                i += 1  # i、j、k都向右移动一位
+                j += 1
+                k += 1
+            else:
+                # 此轮遍历三个数组的值至少有1个不同
+                cur_max = max(arr1[i], arr2[j], arr3[k])
+                # 最大的值所在的指针“等待”小的值指针，除了最大值，其余值的指针向右移动1位
+                if arr1[i] < cur_max:
+                    i += 1
+                if arr2[j] < cur_max:
+                    j += 1
+                if arr3[k] < cur_max:
+                    k += 1
+        return ans
+
+
+if __name__ == "__main__":
+    arr1 = [1,2,3,4,5]
+    arr2 = [1,2,5,7,9]
+    arr3 = [1,3,4,5,8]
+    sol = Solution()
+    result = sol.arraysIntersection(arr1, arr2, arr3)
+    print(result)
