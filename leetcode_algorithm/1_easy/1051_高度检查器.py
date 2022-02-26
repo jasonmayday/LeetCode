e078d715898cd4a7bfff023dc6ce48a24bc0562b
Jiexin Shi <jasonmayday@gmail.com>
2022-02-25 23:06:10 -0500

    Update 1051_高度检查器.py

--- leetcode_algorithm/1_easy/1051_高度检查器.py
+++ leetcode_algorithm/1_easy/1051_高度检查器.py
@@ -11,12 +11,12 @@ https://leetcode-cn.com/problems/height-checker/
 
 示例：
     输入：heights = [1,1,4,2,1,3]
-    输出：3 
+    输出：3
     解释：
     高度：[1,1,4,2,1,3]
     预期：[1,1,1,2,3,4]
     下标 2 、4 、5 处的学生高度不匹配。
-    
+
 示例 2：
     输入：heights = [5,1,2,3,4]
     输出：5
@@ -24,7 +24,7 @@ https://leetcode-cn.com/problems/height-checker/
     高度：[5,1,2,3,4]
     预期：[1,2,3,4,5]
     所有下标的对应学生高度都不匹配。
-    
+
 示例 3：
     输入：heights = [1,2,3,4,5]
     输出：0
