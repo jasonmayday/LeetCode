ad0b4870d5df9254095f0cc606a74721d7737bd5
Jiexin Shi <jasonmayday@gmail.com>
2022-02-25 23:04:55 -0500

    Update 1065_字符串的索引对.py

--- leetcode_algorithm/1_easy/1065_字符串的索引对.py
+++ leetcode_algorithm/1_easy/1065_字符串的索引对.py
@@ -21,3 +21,54 @@ https://leetcode-cn.com/problems/index-pairs-of-a-string/
     按序返回索引对 [i,j]（即，按照索引对的第一个索引进行排序，当第一个索引对相同时按照第二个索引对排序）。
 
 """
+from typing import List
+
+""" 暴力 """
+class Solution:
+    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+        res = []
+        for i in range(len(text)):
+            for j in range(i, len(text)):
+                if text[i:j + 1] in words:
+                    res.append([i, j])
+        return res
+
+""" 前缀树 / 字典树 / Trie树 """
+class Trie:
+    def __init__(self):
+        self.child = dict()
+        self.isWord = False
+
+    def insert(self, word: str):
+        root = self
+        for c in word:
+            if c not in root.child:
+                root.child[c] = Trie()
+            root = root.child[c]
+        root.isWord = True
+
+class Solution:
+    def indexPairs(self, text: str, words: List[str]) -> List[List[int]]:
+        n = len(text)
+        T = Trie()
+        for word in words:
+            T.insert(word)
+        res = []
+        for i, c in enumerate(text):
+            root = T
+            if c not in root.child:
+                continue
+            j = i
+            while j < n and text[j] in root.child:
+                root = root.child[text[j]]
+                if root.isWord == True:
+                    res.append([i, j])
+                j += 1
+        return res
+
+if __name__ == "__main__":
+    text = "thestoryofleetcodeandme"
+    words = ["story","fleet","leetcode"]
+    sol = Solution()
+    result = sol.indexPairs(text, words)
+    print(result)
