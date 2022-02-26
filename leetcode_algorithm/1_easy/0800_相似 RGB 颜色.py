40cbb8229bf18c976d5cd7983e87be126d1b7735
Jiexin Shi <jasonmayday@gmail.com>
2022-02-25 23:18:56 -0500

    Update 0800_相似 RGB 颜色.py

--- leetcode_algorithm/1_easy/0800_相似 RGB 颜色.py
+++ leetcode_algorithm/1_easy/0800_相似 RGB 颜色.py
@@ -28,3 +28,42 @@ RGB 颜色 "#AABBCC" 可以简写成 "#ABC" 。
     对于任何 i > 0，color[i] 都是一个在范围 ['0', 'f'] 内的 16 进制数
 
 """
+
+"""
+    比如说在十进制中，0（00），11，22，33，44，55，都是（10 + 1）的倍数，那么在16进制中，00，11，22，33，都是（16 + 1）的倍数
+    0x11, 0x22, 0x33, 0x44... 都是 0x11 的倍数，0x11 就是 17
+"""
+
+""" 方法一：枚举 """
+class Solution(object):
+    def similarRGB(self, color):
+        def similarity(hex1, hex2):
+            r1, g1, b1 = hex1 >> 16, (hex1 >> 8) % 256, hex1 % 256
+            r2, g2, b2 = hex2 >> 16, (hex2 >> 8) % 256, hex2 % 256
+            return -(r1 - r2)**2 - (g1 - g2)**2 - (b1 - b2)**2
+
+        hex1 = int(color[1:], 16)
+        ans = 0
+        for r in range(16):
+            for g in range(16):
+                for b in range(16):
+                    hex2 = 17 * r * (1 << 16) + 17 * g * (1 << 8) + 17 * b
+                    if similarity(hex1, hex2) > similarity(hex1, ans):
+                        ans = hex2
+
+        return '#{:06x}'.format(ans)
+
+""" 方法二：独立性 + 枚举 """
+class Solution(object):
+    def similarRGB(self, color):
+        def f(comp):
+            q, r = divmod(int(comp, 16), 17)
+            if r > 8: q += 1
+            return '{:02x}'.format(17 * q)
+        return '#' + f(color[1:3]) + f(color[3:5]) + f(color[5:])
+
+if __name__ == "__main__":
+    color = "#09f166"
+    sol = Solution()
+    result = sol.similarRGB(color)
+    print(result)
