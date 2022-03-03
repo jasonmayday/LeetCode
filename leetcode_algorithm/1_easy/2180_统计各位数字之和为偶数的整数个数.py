3987885d972b9831fee45a06adeae72239354fba
Jiexin Shi <jasonmayday@gmail.com>
2022-03-03 12:15:56 -0500

    Update 2180_统计各位数字之和为偶数的整数个数.py

--- leetcode_algorithm/1_easy/2180_统计各位数字之和为偶数的整数个数.py
+++ leetcode_algorithm/1_easy/2180_统计各位数字之和为偶数的整数个数.py
@@ -9,13 +9,13 @@
     输入：num = 4
     输出：2
     解释：
-    只有 2 和 4 满足小于等于 4 且各位数字之和为偶数。    
+    只有 2 和 4 满足小于等于 4 且各位数字之和为偶数。
 
 示例 2：
     输入：num = 30
     输出：14
     解释：
-    只有 14 个整数满足小于等于 4 且各位数字之和为偶数，分别是： 
+    只有 14 个整数满足小于等于 4 且各位数字之和为偶数，分别是：
     2、4、6、8、11、13、15、17、19、20、22、24、26 和 28 。"""
 
 
@@ -32,7 +32,7 @@ class Solution:
                 if eval("+".join(str(n))) % 2 == 0:
                     res += 1
         return res
-    
+
 """用整除来加和"""
 class Solution:
     def countEven(self, num: int) -> int:
@@ -47,7 +47,6 @@ class Solution:
             if nums % 2 == 0:
                 res += 1
         return res
-
 
 if __name__ == "__main__":
     num = 4
