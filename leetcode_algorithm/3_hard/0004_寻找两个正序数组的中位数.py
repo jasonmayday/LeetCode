21585af668da0fc46137e8cac2e1ec1394960942
Jiexin Shi <jasonmayday@gmail.com>
2022-03-03 12:34:20 -0500

    Update 0004_寻找两个正序数组的中位数.py

--- leetcode_algorithm/3_hard/0004_寻找两个正序数组的中位数.py
+++ leetcode_algorithm/3_hard/0004_寻找两个正序数组的中位数.py
@@ -29,27 +29,44 @@ from typing import List
 """ 二分查找 """
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n1 = len(nums1)
-        n2 = len(nums2)
-        if n1 > n2:
-            return self.findMedianSortedArrays(nums2, nums1)
-        k = (n1 + n2 + 1)//2
+
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        len1, len2 = len(nums1), len(nums2)
+
         left = 0
-        right = n1
-        while left < right :
-            m1 = left +(right - left)//2
-            m2 = k - m1
-            if nums1[m1] < nums2[m2-1]:
-                left = m1 + 1
+        right = len1
+        half_len = (len1 + len2 + 1) // 2
+        mid1 = (left + right) // 2
+        mid2 = half_len - mid1
+
+        while left < right:
+            if mid1 < len1 and nums2[mid2-1] > nums1[mid1]:
+                left = mid1 + 1
             else:
-                right = m1
-        m1 = left
-        m2 = k - m1 
-        c1 = max(nums1[m1-1] if m1 > 0 else float("-inf"), nums2[m2-1] if m2 > 0 else float("-inf") )
-        if (n1 + n2) % 2 == 1:
-            return c1
-        c2 = min(nums1[m1] if m1 < n1 else float("inf"), nums2[m2] if m2 <n2 else float("inf"))
-        return (c1 + c2) / 2
+                right = mid1
+            mid1 = (left + right) // 2
+            mid2 = half_len - mid1
+
+        if mid1 == 0:
+            max_of_left = nums2[mid2-1]
+        elif mid2 == 0:
+            max_of_left = nums1[mid1-1]
+        else:
+            max_of_left = max(nums1[mid1-1], nums2[mid2-1])
+
+        if (len1 + len2) % 2 == 1:
+            return max_of_left
+
+        if mid1 == len1:
+            min_of_right = nums2[mid2]
+        elif mid2 == len2:
+            min_of_right = nums1[mid1]
+        else:
+            min_of_right = min(nums1[mid1], nums2[mid2])
+
+        return (max_of_left + min_of_right) / 2
 
 if __name__ == "__main__":
     nums1 = [1,2]
