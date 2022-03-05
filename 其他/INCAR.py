99cb23c2990946d3651bc9458ee055a79a06cca3
Jiexin Shi <jasonmayday@gmail.com>
2022-03-04 20:19:11 -0500

    Update INCAR.py

--- 其他/INCAR.py
+++ 其他/INCAR.py
@@ -1,18 +1,22 @@
-
-
-def repeat(arr, a, b, c, alpha, beta, gamma):
+def repeat(string, a, b, c, alpha, beta, gamma):
+    arr = string.split(" ")
+    arr = [int(x) for x in arr ]
+    arr = [alpha * [i * a for i in arr]] + [beta * [i * b for i in arr]] + [gamma * [i * c for i in arr]]
     new_arr = []
-    return new_arr 
-            += a * [i * a for i in arr] +=
-            b * [i * b for i in arr] +=
-            c * [i * c for i in arr]
+    for i in arr:
+        new_arr += i
+    ans = " ".join([str(_) for _ in new_arr])
+    return ans
 
-arr = [0, 0, 1]
-a = 5
-b = -5
-c = 3
-alpha = 2
-beta = 2
-gamma = 2
-result = repeat(arr, a, b, c, alpha, beta, gamma)
-print(result)
+
+if __name__ == "__main__":
+    string = "0 0 1"
+    a = 5
+    b = -5
+    c = 3
+    alpha = 8
+    beta = 8
+    gamma = 8
+    result = repeat(string, a, b, c, alpha, beta, gamma)
+    print(result)
+
