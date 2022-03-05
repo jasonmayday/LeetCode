a64b93fbf53d916500d8e3abf1a3450d88e9d4f4
Jiexin Shi <jasonmayday@gmail.com>
2022-03-04 19:35:29 -0500

    Update INCAR.py

--- 其他/INCAR.py
+++ 其他/INCAR.py
@@ -1,3 +1,18 @@
-a = 
-b = 
-c = 
+
+
+def repeat(arr, a, b, c, alpha, beta, gamma):
+    new_arr = []
+    return new_arr 
+            += a * [i * a for i in arr] +=
+            b * [i * b for i in arr] +=
+            c * [i * c for i in arr]
+
+arr = [0, 0, 1]
+a = 5
+b = -5
+c = 3
+alpha = 2
+beta = 2
+gamma = 2
+result = repeat(arr, a, b, c, alpha, beta, gamma)
+print(result)
