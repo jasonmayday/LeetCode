a6b3cf31dc4f2317cc3e07c99b2a292f52b23f83
Jiexin Shi <jasonmayday@gmail.com>
2022-03-07 13:20:52 -0500

    Update 2055_蜡烛之间的盘子.py

--- leetcode_algorithm/2_medium/2055_蜡烛之间的盘子.py
+++ leetcode_algorithm/2_medium/2055_蜡烛之间的盘子.py
@@ -31,3 +31,77 @@ https://leetcode-cn.com/problems/plates-between-candles/
     0 <= lefti <= righti < s.length
 
 """
+from typing import List
+
+"""方法一：预处理 + 前缀和"""
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(s)
+        preSum, sum = [0] * n, 0
+        left, l = [0] * n, -1
+        for i, ch in enumerate(s):
+            if ch == '*':
+                sum += 1
+            else:
+                l = i
+            preSum[i] = sum
+            left[i] = l
+
+        right, r = [0] * n, -1
+        for i in range(n - 1, -1, -1):
+            if s[i] == '|':
+                r = i
+            right[i] = r
+
+        ans = [0] * len(queries)
+        for i, (x, y) in enumerate(queries):
+            x, y = right[x], left[y]
+            if x >= 0 and y >= 0 and x < y:
+                ans[i] = preSum[y] - preSum[x]
+        return ans
+
+""" 方法一：预处理 + 前缀和"""
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(s)
+        lcandle = [-1] * n      # lcandle[i] 表示在位置 i 之前(包括 i )，离 i 最近的蜡烛的位置
+        rcandle = [-1] * n      # rcandle[i] 表示在位置 i 之后(包括 i )，离 i 最近的蜡烛的位置
+        preSum = [0] * n        # preSum[i] 表示在位置 i 之前的盘子个数
+
+        if s[0] == '|':         # 处理最左边情况
+            lcandle[0] = 0
+        if s[n - 1] == '|':     # 处理最右边情况
+            rcandle[n - 1] = n - 1
+
+        for i in range(1, n):
+            lcandle[i] = i if s[i] == '|' else lcandle[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            rcandle[i] = i if s[i] == '|' else rcandle[i + 1]
+
+        for i in range(1, n):
+            preSum[i] = preSum[i - 1] + 1 if s[i - 1] == '*' else preSum[i - 1]
+
+        res = []
+
+        for que in queries:
+            left, right = que[0], que[1]
+
+            # 首先确定查询区间内，最左侧和最右侧两个蜡烛的位置
+            lidx = rcandle[left]    # left 右侧离 left 最近的蜡烛位置
+            ridx = lcandle[right]   # right 左侧离 right 最近的蜡烛位置
+
+            if lidx > right or ridx < left:
+                res.append(0)
+                continue
+
+            res.append(preSum[ridx] - preSum[lidx]) # 查询区间内两个蜡烛之间的盘子个数为 prefix_plate[ridx] - prefix_plate[lidx]。
+
+        return res
+
+if __name__ == "__main__":
+    s = "**|**|***|"
+    queries = [[2,5],[5,9]]
+    sol = Solution()
+    result = sol.platesBetweenCandles(s, queries)
+    print(result)
