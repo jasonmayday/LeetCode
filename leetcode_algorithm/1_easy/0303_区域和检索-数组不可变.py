3ae79869b7305bfc73356c9e1a3e85613782becb
Jiexin Shi <jasonmayday@gmail.com>
2022-03-07 12:12:29 -0500

    Update 0303_区域和检索-数组不可变.py

--- leetcode_algorithm/1_easy/0303_区域和检索-数组不可变.py
+++ leetcode_algorithm/1_easy/0303_区域和检索-数组不可变.py
@@ -28,7 +28,6 @@
 from typing import List
 
 class NumArray:
-
     def __init__(self, nums: List[int]):
         self.sums = [0]
         _sums = self.sums
@@ -39,4 +38,10 @@ class NumArray:
     def sumRange(self, i: int, j: int) -> int:
         _sums = self.sums
         return _sums[j + 1] - _sums[i]
+
+if __name__ == "__main__":
+    numArray = NumArray([-2, 0, 3, -5, 2, -1])
+    numArray.sumRange(0, 2)     # return 1 ((-2) + 0 + 3)
+    numArray.sumRange(2, 5)     # return -1 (3 + (-5) + 2 + (-1))
+    numArray.sumRange(0, 5)     # return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
 
