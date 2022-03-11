89b033c95e3fb2e5fabf758e1fc0abab99351685
Jiexin Shi <jasonmayday@gmail.com>
2022-03-11 11:50:46 -0500

    Update 0590_N叉树的后序遍历.py

--- leetcode_algorithm/1_easy/0590_N叉树的后序遍历.py
+++ leetcode_algorithm/1_easy/0590_N叉树的后序遍历.py
@@ -27,38 +27,73 @@ from typing import List
 """递归"""
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        def dfs(root: 'Node') -> None:
-            for child in root.children:
-                dfs(child)
-            value.append(root.val)
-        
-        value = []
-        if root:
-            dfs(root)
-        return value
+        ans = []
+        def dfs(node: 'Node'):
+            if node is None:
+                return
+            for ch in node.children:
+                dfs(ch)
+            ans.append(node.val)
+        dfs(root)
+        return ans
 
 """迭代"""
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        value, s = [], []
-        cur = root
-        while s or cur:
-            while cur:
-                s.append((cur, 0))
-                if cur.children:
-                    cur = cur.children[0]
-                else:
-                    cur = None
-            cur, c = s.pop()
-            if not cur.children or c >= len(cur.children):
-                value.append(cur.val)
-                cur = None
+        if root is None:
+            return []
+        ans = []
+        st = []
+        nextIndex = defaultdict(int)
+        node = root
+        while st or node:
+            while node:
+                st.append(node)
+                if not node.children:
+                    break
+                nextIndex[node] = 1
+                node = node.children[0]
+            node = st[-1]
+            i = nextIndex[node]
+            if i < len(node.children):
+                nextIndex[node] = i + 1
+                node = node.children[i]
             else:
-                c += 1
-                s.append((cur, c))
-                if c < len(cur.children):
-                    cur = cur.children[c]
-                else:
-                    cur = None
-        return value
+                ans.append(node.val)
+                st.pop()
+                del nextIndex[node]
+                node = None
+        return ans
 
+""" 迭代优化 """
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        ans = []
+        st = [root]
+        vis = set()
+        while st:
+            node = st[-1]
+            # 如果当前节点为叶子节点或者当前节点的子节点已经遍历过
+            if len(node.children) == 0 or node in vis:
+                ans.append(node.val)
+                st.pop()
+                continue
+            st.extend(reversed(node.children))
+            vis.add(node)
+        return ans
+    
+""" 利用前序遍历反转 """
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        ans = []
+        st = [root]
+        while st:
+            node = st.pop()
+            ans.append(node.val)
+            st.extend(node.children)
+        ans.reverse()
+        return ans
