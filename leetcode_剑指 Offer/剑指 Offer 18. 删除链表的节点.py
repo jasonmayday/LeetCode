22128f04b00b5dfd60328fdb18397de6f018a612
Jiexin Shi <jasonmayday@gmail.com>
2022-03-26 01:01:17 -0400

    Update 剑指 Offer 18. 删除链表的节点.py

--- leetcode_剑指 Offer/剑指 Offer 18. 删除链表的节点.py
+++ leetcode_剑指 Offer/剑指 Offer 18. 删除链表的节点.py
@@ -23,3 +23,52 @@ https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
 
 """
 
+class ListNode:   # 节点类
+    def __init__(self, val = 0, next = None):  # 初始化节点
+        self.val = val     # val存放数据元素
+        self.next = next   # 保存当前节点中下一个节点的引用
+        # 每个节点包含两个部分，一部分存放数据变量的val，另一部分存放下一个节点的位置信息next。
+    
+    def __str__(self):
+        # 测试基本功能，输出字符串
+        return str(self.val)
+
+'''将传入的数组转化为链表'''
+def create_linked_list(list):
+    head = ListNode(list[0])
+    cur = head
+    for i in range(1, len(list)):
+        cur.next = ListNode(list[i])
+        cur = cur.next
+    return head
+
+# 传入链表头节点，以数组形式返回
+def print_linked_list(head):
+    cur = head
+    list = []
+    while cur:
+        list.append(cur.val)
+        cur = cur.next
+    return list
+
+"""迭代法"""
+class Solution:
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+        if head.val == val:     # 当应删除头节点 head 时
+            return head.next    # 直接返回之后的 head.next 即可。
+        pre = head          # 初始化
+        cur = head.next
+        while cur and cur.val != val:   # 定位节点： 当还有节点，且节点不等于目标值时，继续遍历
+            pre = cur       # 保存当前节点索引，即 pre = cur 。
+            cur = cur.next  # 遍历下一节点，即 cur = cur.next 。
+        if cur:                     # 删除节点： 若 cur 指向某节点，
+            pre.next = cur.next     # 修改节点指向：执行 pre.next = cur.next，跳过目标节点
+        return head     # 返回值： 返回链表头部节点 head 即可。
+
+if __name__ == "__main__":
+    head = [4,5,1,9]
+    val = 5
+
+    sol = Solution()
+    NewLinkedList = sol.deleteNode(head, val)
+    print (print_linked_list(NewLinkedList))
