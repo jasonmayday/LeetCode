198816ae4626b5ebc8b87affd7a24da3471f317e
Jiexin Shi <jasonmayday@gmail.com>
2022-03-27 18:53:55 -0400

    Update 1122_数组的相对排序.py

--- leetcode_algorithm/1_easy/1122_数组的相对排序.py
+++ leetcode_algorithm/1_easy/1122_数组的相对排序.py
@@ -20,12 +20,42 @@ https://leetcode-cn.com/problems/relative-sort-array/
 """
 from typing import List
 
+""" 自定义排序"""
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        def mycmp(x):
+            return rank[x] if x in rank else x
+        
+        n = len(arr2)
+        rank = {x: i - n for i, x in enumerate(arr2)}
+        arr1.sort(key = mycmp)
+        return arr1
+
+""" 计数排序 """
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        upper = max(arr1)
+        frequency = [0] * (upper + 1)
+        for x in arr1:
+            frequency[x] += 1
+        
+        ans = list()
+        for x in arr2:
+            ans.extend([x] * frequency[x])
+            frequency[x] = 0
+        for x in range(upper + 1):
+            if frequency[x] > 0:
+                ans.extend([x] * frequency[x])
+        return ans
+    
+""" 计数排序 """
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         arr = [0 for _ in range(1001)]  # 由于题目说 arr1 的范围在 0-1000，所以生成一个 1001 大小的数组用来存放每个数出现的次数。
         ans = []  # 储存答案的数组。
         for i in range(len(arr1)):  # 遍历 arr1，把整个arr1的数的出现次数储存在 arr 上，arr 的下标对应 arr1 的值，arr 的值对应 arr1 中值出现的次数。
             arr[arr1[i]] += 1       # 如果遇到了这个数，就把和它值一样的下标位置上 +1，表示这个数在这个下标 i 上出现了 1 次。
+        # print('arr: ', arr)       # [0, 1, 3, 2, 1, 0, 1, 1, 0, 1, ...]
         for i in range(len(arr2)):  # 遍历 arr2，现在开始要输出答案了。
             while arr[arr2[i]] > 0: # 如果 arr2 的值在 arr 所对应的下标位置出现次数大于 0，那么就说明 arr 中的这个位置存在值。
                 ans.append(arr2[i]) # 如果存在值，那就把它加到 ans 中，因为要按 arr2 的顺序排序。
@@ -35,7 +65,6 @@ class Solution:
                 ans.append(i)
                 arr[i] -= 1
         return ans  # 返回最终答案。
-
 
 
 if __name__ == "__main__":
