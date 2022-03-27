aba690b5bc0ce84fd55030d5447d88e5a397c08e
Jiexin Shi <jasonmayday@gmail.com>
2022-03-27 19:12:04 -0400

    Update 剑指 Offer II 088. 爬楼梯的最少成本.py

--- leetcode_剑指 Offer/1_easy/剑指 Offer II 088. 爬楼梯的最少成本.py
+++ leetcode_剑指 Offer/1_easy/剑指 Offer II 088. 爬楼梯的最少成本.py
@@ -24,3 +24,56 @@ https://leetcode-cn.com/problems/GzCJIP/
 注意：本题与主站 746 题相同： https://leetcode-cn.com/problems/min-cost-climbing-stairs/
 
 """
+from typing import List
+# https://leetcode-cn.com/problems/min-cost-climbing-stairs/solution/yi-bu-yi-bu-tui-dao-dong-tai-gui-hua-de-duo-chong-/
+
+""" 动态规划 """
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        minCost = [0] * n
+        minCost[1] = min(cost[0], cost[1])
+        for i in range(2, n):
+            minCost[i] = min(minCost[i - 1] + cost[i], minCost[i - 2] + cost[i - 1])
+            # 第0级台阶： minCost[0] = min(cost[-1], cost[0]) = min(0, cost[0]) = 0
+            # 第1级台阶： minCost[1] = min(cost[0], cost[1])
+        return minCost[-1]
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)  # dp[i]表示达到第i级（停止在第i级）台阶上，最小的cost
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        return dp[n]
+
+""" 动态规划 """
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * n
+        dp[0], dp[1] = cost[0], cost[1]
+        # 踏上第i级台阶有两种方法：
+            # 1. 先踏上第i-2级台阶（最小总花费dp[i-2]），再直接迈两步踏上第i级台阶（花费cost[i]），最小总花费dp[i-2] + cost[i]；
+            # 2. 先踏上第i-1级台阶（最小总花费dp[i-1]），再迈一步踏上第i级台阶（花费cost[i]），最小总花费dp[i-1] + cost[i]；
+        # 则dp[i]是上面这两个最小总花费中的最小值。
+        for i in range(2, n):
+            dp[i] = min(dp[i - 2], dp[i - 1]) + cost[i]
+        return min(dp[-2], dp[-1])
+
+""" 动态规划
+    当 i≥2 时，dp[i] 只和 dp[i−1] 与 dp[i−2] 有关，因此可以使用滚动数组的思想，将空间复杂度优化到 O(1)"""
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        prev = curr = 0
+        for i in range(2, n + 1):
+            nxt = min(curr + cost[i - 1], prev + cost[i - 2])
+            prev, curr = curr, nxt
+        return curr
+
+if __name__ == "__main__":
+    cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+    sol = Solution()
+    result = sol.minCostClimbingStairs(cost)
+    print(result)
