39a1f0f3ef7051e377e20f78a971c11f048c97e3
Jiexin Shi <jasonmayday@gmail.com>
2022-03-27 22:32:22 -0400

    Update 0704_二分查找.py

--- leetcode_algorithm/1_easy/0704_二分查找.py
+++ leetcode_algorithm/1_easy/0704_二分查找.py
@@ -5,14 +5,14 @@ https://leetcode-cn.com/problems/binary-search/
 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 
 示例 1:
-输入: nums = [-1,0,3,5,9,12], target = 9
-输出: 4
-解释: 9 出现在 nums 中并且下标为 4
+    输入: nums = [-1,0,3,5,9,12], target = 9
+    输出: 4
+    解释: 9 出现在 nums 中并且下标为 4
 
 示例 2:
-输入: nums = [-1,0,3,5,9,12], target = 2
-输出: -1
-解释: 2 不存在 nums 中因此返回 -1
+    输入: nums = [-1,0,3,5,9,12], target = 2
+    输出: -1
+    解释: 2 不存在 nums 中因此返回 -1
 
 '''
 
