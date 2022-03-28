9f663cb0f2aa7716b625475a4cc95745d54aafc4
Jiexin Shi <jasonmayday@gmail.com>
2022-03-28 00:25:25 -0400

    Update 0054_螺旋矩阵.py

--- leetcode_algorithm/2_medium/0054_螺旋矩阵.py
+++ leetcode_algorithm/2_medium/0054_螺旋矩阵.py
@@ -20,7 +20,56 @@ https://leetcode-cn.com/problems/spiral-matrix/
 """
 from typing import List
 
-"""解法1：动态边界法"""
+""" 模拟 """
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return list()
+        
+        rows, columns = len(matrix), len(matrix[0])         # 行、列
+        visited = [[False] * columns for _ in range(rows)]  # 与输入矩阵大小相同的辅助矩阵, 其中的每个元素表示该位置是否被访问过
+        total = rows * columns  # 总元素数
+        res = [0] * total
+
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]] # 四个方向：右，下，左，上
+        row, column = 0, 0  # 初始位置是矩阵的左上角
+        directionIndex = 0
+        for i in range(total):  # 总步数为总元素数
+            res[i] = matrix[row][column]    # 访问某个元素，加入答案
+            visited[row][column] = True     # 标记该元素为已访问
+            nextRow = row + directions[directionIndex][0]       # 行数的改变与方向向量的下标0有关
+            nextColumn = column + directions[directionIndex][1] # 行数的改变与方向向量的下标1有关
+            if not (0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
+                directionIndex = (directionIndex + 1) % 4    # 初始方向是向右，然后向下，（右，下，左，上 轮换）
+            row += directions[directionIndex][0]
+            column += directions[directionIndex][1]
+        return res
+
+""" 按层模拟
+    定义矩阵的第 k 层是到最近边界距离为 k 的所有顶点"""
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return list()
+        
+        rows, columns = len(matrix), len(matrix[0])
+        order = list()
+        left, right = 0, columns - 1
+        top, bottom = 0, rows - 1
+        while left <= right and top <= bottom:
+            for column in range(left, right + 1):   # 从左到右遍历上侧元素
+                order.append(matrix[top][column])
+            for row in range(top + 1, bottom + 1):  # 从上到下遍历右侧元素
+                order.append(matrix[row][right])
+            if left < right and top < bottom:
+                for column in range(right - 1, left, -1):   # 则从右到左遍历下侧元素
+                    order.append(matrix[bottom][column])
+                for row in range(bottom, top, -1):          # 从下到上遍历左侧元素
+                    order.append(matrix[row][left])
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1     # 进入下一层继续遍历
+        return order
+
+""" 动态边界法 """
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
