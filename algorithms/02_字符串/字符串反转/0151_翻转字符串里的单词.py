ac3aa1e0b2f812c61147bffdde5d27c08d3bd519
Jiexin Shi <jasonmayday@gmail.com>
2022-03-31 15:55:46 -0400

    Update 0151_翻转字符串里的单词.py

--- algorithms/02_字符串/字符串反转/0151_翻转字符串里的单词.py
+++ algorithms/02_字符串/字符串反转/0151_翻转字符串里的单词.py
@@ -53,7 +53,30 @@ class Solution:
         # join   将字符串数组拼成一个字符串。
         # return " ".join(reversed(s.split()))
         return " ".join(s.split()[::-1])
-        
+
+""" 分割 + 倒序"""
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip()       # 删除首尾空格
+        strs = s.split()    # 分割字符串
+        strs.reverse()      # 翻转单词列表
+        return ' '.join(strs) # 拼接为字符串并返回
+
+""" 双指针"""
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = s.strip() # 删除首尾空格
+        i = j = len(s) - 1
+        res = []
+        while i >= 0:
+            while i >= 0 and s[i] != ' ':
+                i -= 1      # 搜索首个空格
+            res.append(s[i + 1: j + 1]) # 添加单词
+            while s[i] == ' ':
+                i -= 1      # 跳过单词间空格
+            j = i           # j 指向下个单词的尾字符
+        return ' '.join(res) # 拼接并返回
+
 """ 自行编写对应的函数 """
 class Solution:
     
