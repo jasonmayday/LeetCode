986555a16eaf21a48b7e04164733439810aaa191
Jiexin Shi <jasonmayday@gmail.com>
2022-03-31 18:53:54 -0400

    Update 0104_二叉树的最大深度.py

--- leetcode_algorithm/1_easy/0104_二叉树的最大深度.py
+++ leetcode_algorithm/1_easy/0104_二叉树的最大深度.py
@@ -18,22 +18,40 @@
 返回它的最大深度 3 。
 
 '''
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-"""深度优先搜索"""
+""" 递归 + 深度优先搜索 """
 class Solution:
-    def maxDepth(self, root):
-        if root is None: 
-            return 0 
-        else:                                           # 如果我们知道了左子树和右子树的最大深度 ll 和 rr，那么该二叉树的最大深度即为 max(l,r)+1
-            left_height = self.maxDepth(root.left)      # 而左子树和右子树的最大深度又可以以同样的方式进行计算。
-            right_height = self.maxDepth(root.right)    # 先递归计算出其左子树和右子树的最大深度
-            return max(left_height, right_height) + 1
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:    # 节点为空，高度为 0
+            return 0
+        left_height = self.maxDepth(root.left)      # 递归计算左子树的最大深度
+        right_height = self.maxDepth(root.right)    # 递归计算右子树的最大深度
+        return max(left_height, right_height) + 1   # 二叉树的最大深度 = 子树的最大深度 + 1（1 是根节点）
+
+""" 迭代 + 深度优先搜索 """
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root == None:    # 节点为空，高度为 0
+            return 0
+        queue = [root]      # 初始化队列和层次
+        depth = 0
+        while queue:        # 当队列不为空
+            n = len(queue)  # 当前层的节点数
             
+            for i in range(n):  # 弹出当前层的所有节点，并将所有子节点入队列
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            depth += 1
+        return depth        # 二叉树最大层次即为二叉树最深深度
 
 if __name__ == "__main__":
     root = TreeNode(1)
@@ -46,11 +64,11 @@ if __name__ == "__main__":
 
     sol = Solution()
     result = sol.maxDepth(root)
-    print (result)  
+    print (result)
     '''
         1
        / \
       2   2
      / \ / \
     3  4 4  3
-    ''' 
+    '''
