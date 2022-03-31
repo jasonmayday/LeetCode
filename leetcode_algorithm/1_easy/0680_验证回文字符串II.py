5ba0ddf56e308f2d11bac63b56fd0905933050f4
Jiexin Shi <jasonmayday@gmail.com>
2022-03-31 01:05:43 -0400

    Update 0680_验证回文字符串II.py

--- leetcode_algorithm/1_easy/0680_验证回文字符串II.py
+++ leetcode_algorithm/1_easy/0680_验证回文字符串II.py
@@ -37,6 +37,26 @@ class Solution(object):
                 return isPalindrome(s[left + 1 : right + 1]) or isPalindrome(s[left: right])
         return True
 
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def checkPalindrome(left, right):
+            i, j = left, right
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return checkPalindrome(left + 1, right) or checkPalindrome(left, right - 1)
+        return True
+
 if __name__ == "__main__":
     s = "abca"
     sol = Solution()
