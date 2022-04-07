2393e0d0f391d84625d4ef942648e8f41666a46b
Jiexin Shi <jasonmayday@gmail.com>
2022-04-07 16:04:58 -0400

    Update 栈实现队列.py

--- algorithms/16_数据结构/数据结构设计——栈与队列/栈实现队列.py
+++ algorithms/16_数据结构/数据结构设计——栈与队列/栈实现队列.py
@@ -27,8 +27,8 @@ https://leetcode-cn.com/problems/implement-queue-using-stacks/
     MyQueue myQueue = new MyQueue();
     myQueue.push(1); // queue is: [1]
     myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
-    myQueue.peek(); // return 1
-    myQueue.pop(); // return 1, queue is [2]
+    myQueue.peek();  // return 1
+    myQueue.pop();   // return 1, queue is [2]
     myQueue.empty(); // return false
 
 提示：
