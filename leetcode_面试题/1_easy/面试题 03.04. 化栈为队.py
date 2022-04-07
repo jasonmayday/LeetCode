5b08ebf82e547cb791cea8c9529a0088632df136
Jiexin Shi <jasonmayday@gmail.com>
2022-04-07 16:25:28 -0400

    Update 面试题 03.04. 化栈为队.py

--- leetcode_面试题/1_easy/面试题 03.04. 化栈为队.py
+++ leetcode_面试题/1_easy/面试题 03.04. 化栈为队.py
@@ -23,34 +23,26 @@ https://leetcode-cn.com/problems/implement-queue-using-stacks-lcci/
 
 class MyQueue:
     def __init__(self):
-        self.s1 = []
-        self.s2 = []
-        self.front = None
+        self.stack1 = []    # stack1 存放新进入的元素
+        self.stack2 = []    # stack2 存放已经完成倒序的元素, 这样B出栈就相当于队列出队
 
-    def push(self, x: int) -> None:
-        """Push element x to the back of queue."""
-        if not self.s1: self.front = x
-        self.s1.append(x)
-        
-    def pop(self) -> int:
-        """Removes the element from in front of queue and returns that element."""
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
-            self.front = None
-        return self.s2.pop()
+    def push(self, x: int) -> None: # 加在 stack1 里面
+        self.stack1.append(x)
 
-    def peek(self) -> int:
-        """Get the front element."""
-        if self.s2:
-            return self.s2[-1]
-        return self.front
+    def pop(self) -> int:           # 移除并返回队列最前端的元素
+        if len(self.stack2) == 0:   # 如果 stack2 为空
+            while self.stack1:      # 那么就从 stack1 里面所有元素出栈并放入 stack2 中
+                self.stack2.append(self.stack1.pop())   # 这样所有A的元素都完成倒序了。之后从B中出栈即可
+        return self.stack2.pop()    # 如果 stack2 中有元素，说明还有完成倒序的元素存在，直接 stack2 出栈
 
-    def empty(self) -> bool:
-        """Returns whether the queue is empty."""
-        if not self.s1 and not self.s2:
-            return True
-        return False
+    def peek(self) -> int:          # 返回队列最前端的元素（不移除）
+        if len(self.stack2) == 0:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+    def empty(self) -> bool:        # 返回队列是否为空
+        return len(self.stack2) == 0 and len(self.stack1) == 0
 
 if __name__ == "__main__":
     obj = MyQueue()
