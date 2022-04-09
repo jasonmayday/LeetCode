f19f108438e12ae0108d8475d5eee09ca05a0ec4
Jiexin Shi <jasonmayday@gmail.com>
2022-04-08 23:24:14 -0400

    Update 面试题 01.02. 判定是否互为字符重排.py

--- leetcode_面试题/1_easy/面试题 01.02. 判定是否互为字符重排.py
+++ leetcode_面试题/1_easy/面试题 01.02. 判定是否互为字符重排.py
@@ -16,4 +16,41 @@ https://leetcode-cn.com/problems/check-permutation-lcci/
     0 <= len(s2) <= 100
 
 """
+from collections import defaultdict
 
+""" 哈希表"""
+class Solution:
+    def CheckPermutation(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        dic = defaultdict(int)
+        for c in s1:
+            dic[c] += 1
+        for c in s2:
+            dic[c] -= 1
+        for val in dic.values():
+            if val != 0:
+                return False
+        return True
+    
+""" 位运算 """
+class Solution:
+    def CheckPermutation(self, s1: str, s2: str) -> bool:
+        length_1= len(s1)
+        if length_1 != len(s2):
+            return False
+
+        result = 0
+        for i in range(length_1):
+            result += 1<< ord(s1[i])
+            result -= 1<< ord(s2[i])
+
+        return result == 0
+
+
+if __name__ == "__main__":
+    s1 = "abc"
+    s2 = "bca"
+    sol = Solution()
+    result = sol.CheckPermutation(s1, s2)
+    print(result)
