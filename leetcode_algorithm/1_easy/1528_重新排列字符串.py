38d3a05e5b2017aa0d79ebfa4cfba1b876c85f56
Jiexin Shi <jasonmayday@gmail.com>
2022-04-08 22:46:59 -0400

    Update 1528_重新排列字符串.py

--- leetcode_algorithm/1_easy/1528_重新排列字符串.py
+++ leetcode_algorithm/1_easy/1528_重新排列字符串.py
@@ -8,26 +8,26 @@ https://leetcode-cn.com/problems/shuffle-string/
 返回重新排列后的字符串。
 
 示例 1：
-输入：s = "codeleet", indices = [4,5,6,7,0,2,1,3]
-输出："leetcode"
-解释：如图所示，"codeleet" 重新排列后变为 "leetcode" 。
+    输入：s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+    输出："leetcode"
+    解释：如图所示，"codeleet" 重新排列后变为 "leetcode" 。
 
 示例 2：
-输入：s = "abc", indices = [0,1,2]
-输出："abc"
-解释：重新排列后，每个字符都还留在原来的位置上。
+    输入：s = "abc", indices = [0,1,2]
+    输出："abc"
+    解释：重新排列后，每个字符都还留在原来的位置上。
 
 示例 3：
-输入：s = "aiohn", indices = [3,1,4,2,0]
-输出："nihao"
+    输入：s = "aiohn", indices = [3,1,4,2,0]
+    输出："nihao"
 
 示例 4：
-输入：s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
-输出："arigatou"
+    输入：s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
+    输出："arigatou"
 
 示例 5：
-输入：s = "art", indices = [1,0,2]
-输出："rat"
+    输入：s = "art", indices = [1,0,2]
+    输出："rat"
 
 提示：
     s.length == indices.length == n
