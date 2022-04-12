fcff8c3f0e15bf5e7ef6506e042d8cc63bb2f572
Jiexin Shi <jasonmayday@gmail.com>
2022-04-12 16:17:59 -0400

    Update 0380_O(1) 时间插入、删除和获取随机元素.py

--- algorithms/16_数据结构/数据结构设计——哈希表/0380_O(1) 时间插入、删除和获取随机元素.py
+++ algorithms/16_数据结构/数据结构设计——哈希表/0380_O(1) 时间插入、删除和获取随机元素.py
@@ -18,13 +18,13 @@ https://leetcode-cn.com/problems/insert-delete-getrandom-o1/
 
     解释
     RandomizedSet randomizedSet = new RandomizedSet();
-    randomizedSet.insert(1); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
-    randomizedSet.remove(2); // 返回 false ，表示集合中不存在 2 。
-    randomizedSet.insert(2); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
-    randomizedSet.getRandom(); // getRandom 应随机返回 1 或 2 。
-    randomizedSet.remove(1); // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
-    randomizedSet.insert(2); // 2 已在集合中，所以返回 false 。
-    randomizedSet.getRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+    randomizedSet.insert(1);    // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+    randomizedSet.remove(2);    // 返回 false ，表示集合中不存在 2 。
+    randomizedSet.insert(2);    // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+    randomizedSet.getRandom();  // getRandom 应随机返回 1 或 2 。
+    randomizedSet.remove(1);    // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+    randomizedSet.insert(2);    // 2 已在集合中，所以返回 false 。
+    randomizedSet.getRandom();  // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
 
 提示：
     -2^31 <= val <= 2^31 - 1
@@ -34,29 +34,33 @@ https://leetcode-cn.com/problems/insert-delete-getrandom-o1/
 """
 
 import random
+""" 数组 + 哈希表
+    数组中存储元素，哈希表中存储每个元素在数组中的下标。
+    插入：val:len(nums) 该元素作为key，末尾作为其索引
+    删除：找到队尾元素，将其索引改为val的索引，然后将val与队尾元素互换，pop出val，删掉字典中val的项
+"""
 class RandomizedSet():
     def __init__(self):
-        self.dict = {}
+        self.dict = {}  # 哈希表的 key 为元素，值为位置
         self.list = []
 
-    def insert(self, val: int) -> bool: # 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，当元素 val 存在时返回 false
+    def insert(self, val: int) -> bool:     # 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，当元素 val 存在时返回 false
         if val in self.dict:
             return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
+        self.dict[val] = len(self.list)     # 插入一个数时，先放到最后，val的索引存入哈希表中【idx = 队尾】
+        self.list.append(val)               # 在数组的末尾添加 val
         return True
         
     def remove(self, val: int) -> bool: # 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，当元素 val 不存在时返回 false
-        if val in self.dict:
-            # move the last element to the place idx of the element to delete
-            last_element = self.list[-1]
-            idx = self.dict[val]
-            self.list[idx], self.dict[last_element] = last_element, idx
-
-            self.list.pop()     # List 删除last_element
-            del self.dict[val]  # dict 删除val
-            return True
-        return False
+        if val not in self.dict:        # val 不在哈希表中，则返回 false
+            return False
+        last_element = self.list[-1]    # 删除一个数时，让最后一个数字去替换: 先找到最后一个元素
+        idx = self.dict[val]            # 找到需要删除的元素的位置
+        self.list[idx] = last_element   # 将数组最后一个元素移到要删除的元素的位置 idx
+        self.dict[last_element] = idx   # 在哈希表中将 last_element 的下标更新为 idx
+        self.list.pop()     # List 删除 last_element (已交换位置，相当于删除输入的元素)
+        del self.dict[val]  # dict 删除 val
+        return True
 
     def getRandom(self) -> int:     # 随机返回现有集合中的一项
         return random.choice(self.list)
