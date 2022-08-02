b5d761a52cf233a78fd991330442fb0a763545c7
Jiexin Shi <jasonmayday@gmail.com>
2022-08-02 14:13:43 -0400

    Update 1374_生成每种字符都是奇数个的字符串.py

--- leetcode_algorithm/1_easy/1374_生成每种字符都是奇数个的字符串.py
+++ leetcode_algorithm/1_easy/1374_生成每种字符都是奇数个的字符串.py
@@ -26,8 +26,8 @@ https://leetcode-cn.com/problems/generate-a-string-with-characters-that-have-odd
 class Solution:
     def generateTheString(self, n: int) -> str:
         if n % 2 == 1:
-            return 'a' * n 
-        else: 
+            return 'a' * n
+        else:
             return 'a' * (n-1) + 'b'
 
 if __name__ == "__main__":
